7d26abecb1e734855ff503d64928a370302298bf
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-04-15 00:49:18 +0200

    5. across_the_planes.py

---
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Module that adds two matrices element-wise
+
+def add_matrices2D(mat1, mat2):
+	
+	# With two matrices as input, the function return the sum of the two matrices as new matrix if the two matrices have the same shape and if not return "None" 
+	
+	if len(mat1) != len(mat2):
+		return None
+	if len(mat1[0]) != len(mat2[0]):
+		return None
+	else:
+		return [[mat1[j][i] + mat2[j][i] for i in range(len(mat1))] for j in range(len(mat1[0]))]
