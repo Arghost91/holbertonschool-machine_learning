a02a63c71cc4fe13cd4a5f5dd403354e2655a11c
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-04-15 01:16:13 +0200

    6. howdy_partner.py

---
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Module that concatenates two arrays
+
+def cat_arrays(arr1, arr2):
+	
+	# With two arrays as input, the function return the concatenates of the two arrays as a new list
+	
+	arr_P = []
+	arr_P += arr1
+	arr_P += arr2
+	return arr_P
