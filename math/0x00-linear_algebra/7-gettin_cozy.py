8e35e2d3b7347f529744bffe3f7ec4ecba660c74
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-04-15 22:52:04 +0200

    Update 7-gettin_cozy.py

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -3,7 +3,7 @@
 
 
 def cat_matrices2D(mat1, mat2, axis=0):
-    
+    # With 2 matrices and a value of axis the function return the concatonate
     if axis == 0:
         mat_P = [row.copy() for row in mat1] + mat2.copy()
         return mat_P
