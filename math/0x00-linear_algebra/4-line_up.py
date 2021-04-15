eafdc3110ecfcf50c99d30b8ba34d97c77241e95
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-04-15 16:45:13 +0200

    Update 4-line_up.py

--- math/0x00-linear_algebra/4-line_up.py
+++ math/0x00-linear_algebra/4-line_up.py
@@ -2,11 +2,13 @@
 
 # Module that adds two arrays element-wise
 
+
 def add_arrays(arr1, arr2):
-	
-	# With two arrays as input, the function return the sum of the two arrays as new list if the two arrays have the same shape 
-	
-	if len(arr1) != len(arr2):
-		return None 
-	else:
-		return [arr1[i] + arr2[i] for i in range(len(arr1))]
+
+    """With two arrays as input, the function return the sum 
+       of the two arrays as new list if the two arrays have the same shape""" 
+
+    if len(arr1) != len(arr2):
+        return None 
+    else:
+        return [arr1[i] + arr2[i] for i in range(len(arr1))]
