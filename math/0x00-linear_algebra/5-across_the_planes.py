03cf7fdf64d9108f1f2148bed4ef6aabd54ede64
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-04-15 22:01:45 +0200

    Update 5-across_the_planes.py

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -12,5 +12,6 @@ def add_matrices2D(mat1, mat2):
         return None
     elif len(mat1[0]) != len(mat2[0]):
         return None
-    return [[mat1[i][j] + mat2[i][j]
-             for i in range(len(mat1))] for j in range(len(mat1[0]))]
+    else:
+        return [[mat1[i][j] + mat2[i][j]
+                 for i in range(len(mat1))] for j in range(len(mat1[0]))]
