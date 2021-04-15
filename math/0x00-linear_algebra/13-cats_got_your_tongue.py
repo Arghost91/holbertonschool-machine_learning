ab684a63849b79ddf90b201b8805c160fd1e22ce
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-04-15 21:42:56 +0200

    Update 13-cats_got_your_tongue.py

--- math/0x00-linear_algebra/13-cats_got_your_tongue.py
+++ math/0x00-linear_algebra/13-cats_got_your_tongue.py
@@ -2,6 +2,6 @@
 # Module that concatenates two matrices along a specific axis
 
 import numpy as np
-def np_cat(mat1, mat2, axis=0)
-    mat_P = np.concatenate((mat1, mat2), axis = axis)
+def np_cat(mat1, mat2, axis=0):
+    mat_P = np.concatenate((mat1, mat2), axis)
     return mat_P
