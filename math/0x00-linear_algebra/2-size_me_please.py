664ce3fba25e5038a051f6fd73eeb2a901d4ef8c
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-04-15 16:35:23 +0200

    Update 2-size_me_please.py

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -4,7 +4,7 @@
 
 
 def matrix_shape(matrix):
-  # With matrix as input, the function return the shape of a matrix  
+    # With matrix as input, the function return the shape of a matrix
     if type(matrix[0][0]) is list:
         return[len(matrix), len(matrix[0]), len(matrix[0][0])]
     else:
