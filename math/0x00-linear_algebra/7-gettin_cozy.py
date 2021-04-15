5d6d570426de9d3e8da3611eaeb5fe4659441638
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-04-15 22:46:35 +0200

    Update 7-gettin_cozy.py

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -5,8 +5,7 @@
 def cat_matrices2D(mat1, mat2, axis=0):
     
     if axis == 0:
-        mat_P = mat1.copy()
-        mat_P += mat2.copy()
+        mat_P = mat1.copy() + mat2.copy()
         return mat_P
     elif axis == 1:
         mat_P = [mat1[i] + mat2[i] for i in range(len(mat1))]
