1efffbea0720f5c983752c3493735bcd2add1c02
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-04-15 22:20:51 +0200

    Create 7-gettin_cozy.py

---
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# Module that concatenates two matrices along a specific axis
+
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    
+    if axis == 0:
+        mat_P = mat1.copy
+        mat_P += mat2
+        return mat_P
+    elif axis == 1:
+        for i in range(len(mat1)):
+            mat_P = mat1[i] + mat2[i]
+            return mat_P
+    else:
+        return None
+        
