c1ef38baf9a83699127f0cf6cf107c6c453372bf
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-04-15 20:20:22 +0200

    Create 11-the_western_exchange.py

---
+++ math/0x00-linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# Module that transposes matrix:
+
+
+def np_transpose(matrix):
+    # With two matrices as input, the function return the sum of
+    mat_P = matrix.transpose()
+    return mat_P
