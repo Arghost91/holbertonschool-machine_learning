44866360813b569da8ae278269c702e19b2265cf
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-04-15 18:42:32 +0200

    Update 8-ridin_bareback.py

--- math/0x00-linear_algebra/8-ridin_bareback.py
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -11,5 +11,7 @@ def mat_mul(mat1, mat2):
             for j in range(len(mat2[0])):
                 for k in range(len(mat1[0])):
                     mat_P[i][j] += mat1[i][k] * mat2[k][j]
-    return mat_P
-  
+        return mat_P
+    else:
+        return None
+    
