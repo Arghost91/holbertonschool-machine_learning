db8cc6ff4be1d655e76023126f3335174d7481b9
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-04-15 18:20:15 +0200

    Update 8-ridin_bareback.py

--- math/0x00-linear_algebra/8-ridin_bareback.py
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -5,8 +5,10 @@
 
 def mat_mul(mat1, mat2):
     # With two matrices as input, the function return the sum of the two matrices as new list if the two matrices have the same shape 
-    if len(mat1) == len(mat[2]):
-        mat_P += mat1[i][k] * mat2[k][j] for i in range(len(mat1))
-                 for j in range(len(mat2[0])) for k in range(len(mat1[0]))
+    if len(mat1[0]) == len(mat2):
+        for i in range(len(mat1)):
+            for j in range(len(mat2[0])):
+                for k in range(len(mat1[0])):
+                    mat_P[i][j] += mat1[i][k] * mat2[k][j]
     return mat_P
   
