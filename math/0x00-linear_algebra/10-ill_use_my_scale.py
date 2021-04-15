7c318c45ff246b718188c17b22ad8b780880adc7
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-04-15 20:31:46 +0200

    Update 10-ill_use_my_scale.py

--- math/0x00-linear_algebra/10-ill_use_my_scale.py
+++ math/0x00-linear_algebra/10-ill_use_my_scale.py
@@ -1,6 +1,8 @@
-#!/usr/bin/env python3
+"""
+Module that calculates the shape of a numpy.ndarray:
+"""
 
-# Module that calculates the shape of a numpy.ndarray:
+#!/usr/bin/env python3
 
 
 def np_shape(matrix):
