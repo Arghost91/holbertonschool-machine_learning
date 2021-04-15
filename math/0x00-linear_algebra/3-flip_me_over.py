f0972e04f3ad68dfcfe50bbfbf94c5bda3e0717c
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-04-15 16:41:58 +0200

    Update 3-flip_me_over.py

--- math/0x00-linear_algebra/3-flip_me_over.py
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -5,8 +5,8 @@
 
 def matrix_transpose(matrix):
 
-	# With matrix as input, the function return the transpose of a 2D matrix
-	
-	matrix_P = [[matrix[i][j]for i in range(len(matrix))]
-		for j in range(len(matrix[0]))]
-	return matrix_P	
+    # With matrix as input, the function return the transpose of a 2D matrix
+
+    matrix_P = [[matrix[i][j]for i in range(len(matrix))]
+                for j in range(len(matrix[0]))]
+    return matrix_P	
