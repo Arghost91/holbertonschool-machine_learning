ba54d7e5cf7ddcb1ff762122b19e0955a43cbf47
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-04-19 14:17:33 +0200

    Update 5-all_in_one.py

--- math/0x01-plotting/5-all_in_one.py
+++ math/0x01-plotting/5-all_in_one.py
@@ -25,18 +25,22 @@ y32 = np.exp((r3 / t32) * x3)
 np.random.seed(5)
 student_grades = np.random.normal(68, 15, 50)
 
+# Title figure
 plt.suptitle('All in One')
 
+# Line Graph
 plt.subplot2grid((3, 2), (0, 0))
 plt.plot(y0, 'r-')
 plt.xlim(0, 10)
 
+# Scatter
 plt.subplot2grid((3, 2), (0, 1))
 plt.plot(x1, y1, 'm.')
 plt.xlabel('Height(in)', fontsize='x-small')
 plt.ylabel('Weight(lbs)', fontsize='x-small')
 plt.title("Men's Height vs Weight", fontsize='x-small')
 
+# Change of scale
 plt.subplot2grid((3, 2), (1, 0))
 plt.plot(x2, y2)
 plt.xlabel('Time (years)', fontsize='x-small')
@@ -45,6 +49,7 @@ plt.title('Exponential Decay of C-14', fontsize='x-small')
 plt.yscale('log')
 plt.xlim(0, 28650)
 
+# Two is better than one
 plt.subplot2grid((3, 2), (1, 1))
 plt.plot(x3, y31, 'r--', x3, y32, 'g-')
 plt.xlabel('Time (years)', fontsize='x-small')
@@ -54,7 +59,8 @@ plt.xlim(0, 20000)
 plt.ylim(0, 1)
 plt.legend(["C-14", "Ra-226"], loc="upper right")
 
-plt.subplot2grid((3, 2), (2,0), colspan=2)
+# Frequency
+plt.subplot2grid((3, 2), (2, 0), colspan=2)
 plt.hist(student_grades, bins=(range(0, 110, 10)), edgecolor='black')
 plt.xlim(0, 100)
 plt.ylim(0, 30)
