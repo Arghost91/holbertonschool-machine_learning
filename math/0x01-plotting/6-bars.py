e14951b80aa4260322d6805a06734e8d69323e2e
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-04-19 15:09:39 +0200

    Update 6-bars.py

--- math/0x01-plotting/6-bars.py
+++ math/0x01-plotting/6-bars.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 np.random.seed(5)
 fruit = np.random.randint(0, 20, (4,3))
 
-bars = np.add(bars1, bars2, bars3).tolist()
+bars = np.add(fruit).tolist()
 
 names = ['Farrah', 'Fred', 'Felicia']
 fruits_1 = ['apples', 'bananas', 'oranges', 'peaches']
@@ -14,10 +14,10 @@ colors = ['red', 'yellow', '#ff8000', '#ffe5b4']
 #for i in range(len(fruit)):
 #    plt.bar(names, fruit[i], color=colors[i], width=0.5)
     
-plt.bar(names, fruit[0], bars1, color='red', label='apples', width=0.5)
-plt.bar(names, fruit[0], bars2, bottom=bars1, color='yellow', label='bananas', width=0.5)
-plt.bar(names, fruit[0], bars3, bottom=bars2, color='#ff8000', label='oranges', width=0.5)
-plt.bar(names, fruit[0], bars4, bottom=bars2, color='#ffe5b4', label='peaches', width=0.5)
+plt.bar(names, fruit[0], color='red', label='apples', width=0.5)
+plt.bar(names, fruit[1], bottom=fruit[:1], color='yellow', label='bananas', width=0.5)
+plt.bar(names, fruit[2], bottom=fruit[:2], color='#ff8000', label='oranges', width=0.5)
+plt.bar(names, fruit[3], bottom=fruit[:3], color='#ffe5b4', label='peaches', width=0.5)
 
 
 plt.ylabel('Quantity of Fruit')
