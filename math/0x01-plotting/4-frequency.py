c37de10d06233888935d3d36dfdfbead1d0bea11
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-04-19 13:05:13 +0200

    Update 4-frequency.py

--- math/0x01-plotting/4-frequency.py
+++ math/0x01-plotting/4-frequency.py
@@ -6,6 +6,8 @@ np.random.seed(5)
 student_grades = np.random.normal(68, 15, 50)
 
 plt.hist(student_grades, bins=10, edgecolor='black')
+plt.xlim(0, 100)
+plt.ylim(0, 30)
 plt.xlabel('Grades')
 plt.ylabel('Number of Students')
 plt.title('Project A')
