70c0fceca83e1aaa76b98c5c2747882b6c149db1
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-04-19 14:47:48 +0200

    Update 6-bars.py

--- math/0x01-plotting/6-bars.py
+++ math/0x01-plotting/6-bars.py
@@ -9,13 +9,12 @@ names = ['Farrah', 'Fred', 'Felicia']
 fruits_1 = ['apples', 'bananas', 'oranges', 'peaches']
 colors = ['red', 'yellow', '#ff8000', '#ffe5b4']
 
-plt.bar(fruit[0], 'apples', color=colors[0], width=0.5)
-plt.bar(fruit[1], 'bananas', color=colors[1], width=0.5)
-plt.bar(fruit[2], 'oranges', color=colors[2], width=0.5)
-plt.bar(fruit[3], 'peaches', color=colors[3], width=0.5)
+plt.bar(names, fruit[0], 'apples', color=colors[0], width=0.5, label)
+plt.bar(names, fruit[1], 'bananas', color=colors[1], width=0.5)
+plt.bar(names, fruit[2], 'oranges', color=colors[2], width=0.5)
+plt.bar(names, fruit[3], 'peaches', color=colors[3], width=0.5)
 plt.legend(["apples", "bananas", "oranges", "peaches"], loc="upper right")
 
-plt.xticks(fruit, 'Farrah', 'Fred', 'Felicia')
 plt.ylabel('Quantity of Fruit')
 plt.yticks(np.arange(0, 90, 10))
 
