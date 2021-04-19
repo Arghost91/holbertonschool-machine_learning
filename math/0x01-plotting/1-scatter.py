7e676426521afdfd5bc3aca6777bb7236344f462
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-04-19 11:11:39 +0200

    Update 1-scatter.py

--- math/0x01-plotting/1-scatter.py
+++ math/0x01-plotting/1-scatter.py
@@ -8,7 +8,7 @@ np.random.seed(5)
 x, y = np.random.multivariate_normal(mean, cov, 2000).T
 y += 180
 
-plt.plot(x, y, 'mo')
+plt.plot(x, y, 'm.')
 plt.xlabel('Height (in)')
 plt.ylabel('Weight (lbs)')
 
