874a34f44b4214c7ede9cdfa295056d0ef55825a
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-04-19 21:20:22 +0200

    Update 6-bars.py

--- math/0x01-plotting/6-bars.py
+++ math/0x01-plotting/6-bars.py
@@ -3,16 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 np.random.seed(5)
-fruit = np.random.randint(0, 20, (4,3))
+fruit = np.random.randint(0, 20, (4, 3))
 
 names = ['Farrah', 'Fred', 'Felicia']
 fruits_1 = ['apples', 'bananas', 'oranges', 'peaches']
 colors = ['red', 'yellow', '#ff8000', '#ffe5b4']
 
-# bot1 = 
-#for i in range(len(fruit)):
-#    plt.bar(names, fruit[i], color=colors[i], width=0.5)
-    
 plt.bar(names, fruit[0], color='red', label='apples', width=0.5)
 plt.bar(names, fruit[1], bottom=(fruit[0]), color='yellow', label='bananas', width=0.5)
 plt.bar(names, fruit[2], bottom=(fruit[0]+fruit[1]), color='#ff8000', label='oranges', width=0.5)
