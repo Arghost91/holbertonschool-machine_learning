05a507f5592ef5f278dd674692b29c9bb4e99460
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-04-19 21:32:20 +0200

    Update 100-gradient.py

--- math/0x01-plotting/100-gradient.py
+++ math/0x01-plotting/100-gradient.py
@@ -8,8 +8,9 @@ x = np.random.randn(2000) * 10
 y = np.random.randn(2000) * 10
 z = np.random.rand(2000) + 40 - np.sqrt(np.square(x) + np.square(y))
 
-plt.scattter(x, y, z)
+plt.scatter(x, y, z)
 plt.xlabel('x coordinate (m)')
 plt.ylabel('y coordinate (m)')
 plt.title('Mountain Elevation')
 plt.colorbar(label='elevation (m)')
+plt.plot()
