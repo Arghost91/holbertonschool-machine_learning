92a4ae624b062acfcb328e01c6a03493e4e7729e
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-04-20 15:26:35 +0200

    Update 17-integrate.py

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -1,4 +1,3 @@
-  
 #!/usr/bin/env python3
 """
 Function that integrates a polinomy
@@ -7,19 +6,19 @@ Function that integrates a polinomy
 
 def poly_integral(poly, C=0):
     """
-    Return de integrate of poly, if poly is 
+    Return de integrate of poly, if poly is
     different to 0 or C is an int
     """
-    if (poly==0) or (type(C) is not int):
+    if (poly == 0) or (type(C) is not int):
         return None
     else:
         integ = poly.copy()
         i = 0
         for i in range(len(poly)):
-            if poly[i]%(i+1)==0:
+            if poly[i] % (i+1) == 0:
                 integ[i] = int(poly[i]/(i+1))
             else:
                 integ[i] = poly[i]/(i+1)
             i += 1
-        integ.insert(0,C)
+        integ.insert(0, C)
         return integ
