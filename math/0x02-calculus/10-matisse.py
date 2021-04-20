a996e630c4d5c84e847d75c45482dd9f29df0721
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-04-20 13:41:34 +0200

    Create 10-matisse.py

---
+++ math/0x02-calculus/10-matisse.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+
+"""
+
+
+def poly_derivative(poly):
+    if poly==0:
+        return None
+    else:
+        n = len(polly)-1
+        der[n]
+        i = 1
+        for i<=n:
+            der[i] = polly[i]*i
+            i += 1
+        return der
