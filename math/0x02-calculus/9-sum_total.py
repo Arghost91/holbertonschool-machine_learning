b33804efc3ea450aad22f61ae603e89ee079f0d6
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-04-20 13:06:40 +0200

    Create 9-sum_total.py

---
+++ math/0x02-calculus/9-sum_total.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Function that returns the cuadratic sum
+"""
+
+
+def summation_i_squared(n):
+    """
+    Calculate the cuadratic sum for n
+    """
+    if ((n<1) or (type(n) is not int)):
+        return None
+    else:
+        cacl = (n*(n+1)*(2*n+1))/6
+        return int(calc)
+return calc
