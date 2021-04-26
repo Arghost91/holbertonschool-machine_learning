d72091c771d96ba44aad86560eb732e0f8e0daa3
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-04-27 01:52:45 +0200

    Create normal.py

---
+++ math/0x03-probability/normal.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+
+"""
+
+class Normal:
+    
+    def __init__(self, data=None, mean=0., stddev=1.):
+        
+        self.mean = float(mean)
+        self.stddev = float(stddev)
+        if data is none:
+            if stddev <= 0:
+                raise ValueError("stddev must be a positive value")
+        else:
+            self.mean = sum(data)/len(data)
+            for i in range len(data):
+                var = sum(data[i] - self.mean)**2
+            self.stddev = (var/len(data))**(1/2)
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+            elif data < 2:
+                raise ValueError("data must contain multiple values")
