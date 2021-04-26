4a31874d4b0a9b07504b71fa5235ff079252a781
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-04-26 12:09:35 +0200

    Update exponential.py

--- math/0x03-probability/exponential.py
+++ math/0x03-probability/exponential.py
@@ -10,9 +10,9 @@ def __init__(self, data=None, lambtha=1.):
     if data is None:
           if lambtha<0:
               raise ValueError("lambtha must be a positive value")
-      else:
-          self.lambtha = sum(data)/len(data)
-          if type(data) is not list:
-              raise TypeError("data must be a list")
-          elif len(data)<2:
-              raise ValueError("data must contain multiple values")         
+    else:
+        self.lambtha = sum(data)/len(data)
+        if type(data) is not list:
+            raise TypeError("data must be a list")
+        elif len(data)<2:
+            raise ValueError("data must contain multiple values")         
