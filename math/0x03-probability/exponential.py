09d8005a93723ad5a5840a6cb1149e17ad13469a
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-04-27 01:16:55 +0200

    Update exponential.py

--- math/0x03-probability/exponential.py
+++ math/0x03-probability/exponential.py
@@ -4,16 +4,24 @@
 """
 
 class Exponential:
-  def __init__(self, data=None, lambtha=1.):
+    def __init__(self, data=None, lambtha=1.):
     
     
-      self.lambtha = float(lambtha)
-      if data is None:
-          if lambtha<0:
-              raise ValueError("lambtha must be a positive value")
-      else:
-          self.lambtha = 1/(sum(data)/len(data))
-          if type(data) is not list:
-              raise TypeError("data must be a list")
-          elif len(data)<2:
-              raise ValueError("data must contain multiple values")         
+        self.lambtha = float(lambtha)
+        if data is None:
+            if lambtha<0:
+                raise ValueError("lambtha must be a positive value")
+        else:
+            self.lambtha = 1/(sum(data)/len(data))
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+            elif len(data)<2:
+                raise ValueError("data must contain multiple values")         
+    
+    def pdf(self, x):
+        
+        e = 2.7182818285
+        if x < 0:
+            return 0
+        else:
+            return self.lamptha*(e**-(self.lamptha*x)
