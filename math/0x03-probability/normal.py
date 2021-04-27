fec8f047a27ff89b00c5bf935eee4845c86285d4
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-04-27 11:05:18 +0200

    Update normal.py

--- math/0x03-probability/normal.py
+++ math/0x03-probability/normal.py
@@ -6,7 +6,9 @@
 class Normal:
     
     def __init__(self, data=None, mean=0., stddev=1.):
-        
+
+        self.mean = float(mean)
+        self.stddev = float(stddev)
         if data is None:
             if stddev <= 0:
                 raise ValueError("stddev must be a positive value")
@@ -19,5 +21,4 @@ class Normal:
                 raise TypeError("data must be a list")
             elif len(data) < 2:
                 raise ValueError("data must contain multiple values")
-        self.mean = float(mean)
-        self.stddev = float(stddev)
+
