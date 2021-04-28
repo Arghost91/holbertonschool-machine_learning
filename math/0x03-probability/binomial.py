505f5129f808baf4dbf5c57d8d748357b46d357b
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-04-28 13:02:48 +0200

    Create binomial.py

---
+++ math/0x03-probability/binomial.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+class Binomial:
+
+    def __init__(self, data=None, n=1, p=0.5):
+      
+        self.n = int(n)
+        self.p = float(p)
+        
+        if data is None:
+            if n < 1:
+                raise ValueError("n must be a positive value")
+            elif p >= 1 or p <= 0:
+                raise ValueError("p must be greater than 0 and less than 1")
+        else:
+            mean = sum(data) / len(data)
+            var = sum([(x  - self.mean) ** 2 for x in data]) / len(data)
+            self.p = 1 - (var / mean)
+            self.n = int(round(mean / self.p))
+            self.p = mean / self.n
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+            elif len(data) < 2:
+                raise ValueError("data must contain multiple values")
