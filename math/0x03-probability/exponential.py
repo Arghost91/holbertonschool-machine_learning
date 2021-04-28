fd8258aea96caefe0d96a09cf071f8e17fc88a32
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-04-28 14:05:50 +0200

    Update exponential.py

--- math/0x03-probability/exponential.py
+++ math/0x03-probability/exponential.py
@@ -1,35 +1,43 @@
 #!/usr/bin/env python3
 """
-
+Class that represents a Exponential distribution
 """
 
 class Exponential:
+    """
+    Class for Exponential
+    """
     def __init__(self, data=None, lambtha=1.):
-    
-    
+        """
+        Class Constructor
+        """
         self.lambtha = float(lambtha)
         if data is None:
-            if lambtha<0:
+            if lambtha < 0:
                 raise ValueError("lambtha must be a positive value")
         else:
-            self.lambtha = 1/(sum(data)/len(data))
+            self.lambtha = 1 / (sum(data) / len(data))
             if type(data) is not list:
                 raise TypeError("data must be a list")
-            elif len(data)<2:
+            elif len(data) < 2:
                 raise ValueError("data must contain multiple values")         
     
     def pdf(self, x):
-        
+        """
+        pdf (Probability Density Function) for a Exponential Distribution
+        """
         e = 2.7182818285
         if x < 0:
             return 0
         else:
-            return self.lambtha*(e**-(self.lambtha*x))
+            return self.lambtha * (e ** -(self.lambtha * x))
         
     def cdf(self, x):
-        
+        """
+        cdf (Cumulative Distribution Function) for a Exponential Distribution
+        """
         e = 2.7182818285
         if x < 0:
             return 0
         else:
-            return 1-(e**-(self.lambtha*x))
+            return 1 - (e ** -(self.lambtha * x))
