b2161837bfc376161118b9c59449ecd8d6d1336e
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-04-28 13:04:03 +0200

    Update binomial.py

--- math/0x03-probability/binomial.py
+++ math/0x03-probability/binomial.py
@@ -14,7 +14,7 @@ class Binomial:
                 raise ValueError("p must be greater than 0 and less than 1")
         else:
             mean = sum(data) / len(data)
-            var = sum([(x  - self.mean) ** 2 for x in data]) / len(data)
+            var = sum([(x  - mean) ** 2 for x in data]) / len(data)
             self.p = 1 - (var / mean)
             self.n = int(round(mean / self.p))
             self.p = mean / self.n
