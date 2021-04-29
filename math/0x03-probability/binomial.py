629e3a47d573058c167b270121aecb60f7cef0e5
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-04-29 09:12:52 +0200

    Update binomial.py

--- math/0x03-probability/binomial.py
+++ math/0x03-probability/binomial.py
@@ -20,15 +20,15 @@ class Binomial:
             elif p >= 1 or p <= 0:
                 raise ValueError("p must be greater than 0 and less than 1")
         else:
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+            elif len(data) < 2:
+                raise ValueError("data must contain multiple values")
             mean = sum(data) / len(data)
             var = sum([(x - mean) ** 2 for x in data]) / len(data)
             self.p = 1 - (var / mean)
             self.n = int(round(mean / self.p))
             self.p = mean / self.n
-            if type(data) is not list:
-                raise TypeError("data must be a list")
-            elif len(data) < 2:
-                raise ValueError("data must contain multiple values")
 
     def pmf(self, k):
         """
