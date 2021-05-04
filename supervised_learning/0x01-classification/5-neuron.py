9b20e53613261ea72a1bec6da4694b6145ace766
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-05-04 14:48:55 +0200

    Create 5-neuron.py

---
+++ supervised_learning/0x01-classification/5-neuron.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+class Neuron:
+    
+    def __init__(self, nx):
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.normal(size=(1, nx))
+        self.__b = 0
+        self.__A = 0
+        
+    @property
+    def W(self):
+        return self.__W
+    @property
+    def b(self):
+        return self.__b
+    @property
+    def A(self):
+        return self.__A
+    
+    def forward_prop(self, X):
+        r = np.dot(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-r))
+        return self.__A
+      
+    def cost(self, Y, A):
+        m = len(Y[0])
+        return (-1 / m) * np.sum(np.multiply(Y, np.log(A)) + np.multiply((1 - Y), np.log(1.0000001 - A)))
+      
+    def evaluate(self, X, Y):
+        A = self.forward_prop(X)
+        pred = np.where(A >= 0.5, 1, 0)
+        cost = self.cost(Y, A)
+        return pred, cost
+      
+    def gradient_descent(self, X, Y, A, alpha=0.05):
+        m = len(Y[0])
+        dW = (1 / m) * (np.dot(X.T, (A - Y.T).T))
+        db = (1 / m) * (np.sum(A - Y.T))
+        self.__W -= (alpha * (dW.T))
+        self.__b -= (alpha * db)
