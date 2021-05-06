e19fd7f53be92ac69a3e9e7f4683f8d343fcf713
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-05-06 19:49:40 +0200

    Create 16-deep_neural_network.py

---
+++ supervised_learning/0x01-classification/16-deep_neural_network.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+class DeepNeuralNetwork:
+    
+    def __init__(self, nx, layers):
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(layers) is not list:
+            raise TypeError("layers must be a list of positive integers")
+        self.L = len(Layers)
+        self.cache = {}
+        self.weights = {}
+        if layers[0] < 0:
+            raise TypeError("layers must be a list of positive integers")
+        for i in range(1, self.L):
+            if layers[i] < 0:
+                raise TypeError("layers must be a list of positive integers")
+            self.weights['W' + str(i)] = np.random.randn(layers[i], layers[i - 1]) * 0.01
+            self.weights['b' + str(i)] = np.zeros((layers[i], 1))
