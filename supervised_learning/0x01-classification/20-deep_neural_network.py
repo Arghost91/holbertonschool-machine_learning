4f79d6b710f7b46e1969e3d1e387f04d53e73302
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-05-07 13:10:39 +0200

    Create 20-deep_neural_network.py

---
+++ supervised_learning/0x01-classification/20-deep_neural_network.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+class DeepNeuralNetwork:
+    
+    def __init__(self, nx, layers):
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(layers) is not list:
+            raise TypeError("layers must be a list of positive integers")
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+
+        for i in range(self.L):
+            if layers[i] < 0:
+                raise TypeError("layers must be a list of positive integers")
+            if i == 0:
+                self.weights['W' + str(i+1)] = np.random.randn(layers[i], nx) * np.sqrt(2 / nx)
+            else:
+                self.weights['W' + str(i+1)] = np.random.randn(layers[i], layers[i - 1]) * np.sqrt(2 / (layers[i - 1]))
+            self.weights['b' + str(i+1)] = np.zeros((layers[i], 1))
+
+    @property
+    def L(self):
+        return self.__L
+    @property
+    def cache(self):
+        return self.__cache
+    @property
+    def weights(self):
+        return self.__weights
+    
+    def forward_prop(self, X):
+        self.__cache["A0"] = X
+        for i in range(self.__L):
+            r = np.dot(self.__weights["W" + str(i+1)], self.cache["A" + str(i)]) + self.__weights["b" + str(i+1)]
+            self.__cache["A" + str(i+1)] = 1 / (1 + np.exp(-r))
+        return self.__cache["A" + str(self.__L)], self.__cache
+      
+    def cost(self, Y, A):
+        m = len(Y[0])
+        return (-1 / m) * np.sum(np.multiply(Y, np.log(A)) + np.multiply((1 - Y), np.log(1.0000001 - A)))
+      
+    def evaluate(self, X, Y):
+        self.forward_prop(X)
+        pred = np.where(self.__cache >= 0.5, 1, 0)
+        cost = self.cost(Y, self.__cache)
+        return pred, cost
