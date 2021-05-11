67bf110b253f102675364bf37febaedab46b13ea
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-05-12 01:58:24 +0200

    Update 16-deep_neural_network.py

--- supervised_learning/0x01-classification/16-deep_neural_network.py
+++ supervised_learning/0x01-classification/16-deep_neural_network.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 
 import numpy as np
+"""
+Defines a deep neural network performing binary classification
+"""
+
 
 class DeepNeuralNetwork:
-    
+    """
+    Class that defines a deep neural network performing binary classification
+    """
     def __init__(self, nx, layers):
         if type(nx) is not int:
             raise TypeError("nx must be an integer")
@@ -14,13 +20,12 @@ class DeepNeuralNetwork:
         self.L = len(layers)
         self.cache = {}
         self.weights = {}
-        ###if layers[0] < 0:
-        ###    raise TypeError("layers must be a list of positive integers")
         for i in range(self.L):
             if layers[i] < 0:
                 raise TypeError("layers must be a list of positive integers")
             if i == 0:
                 self.weights['W' + str(i+1)] = np.random.randn(layers[i], nx) * np.sqrt(2 / nx)
             else:
-                self.weights['W' + str(i+1)] = np.random.randn(layers[i], layers[i - 1]) * np.sqrt(2 / (layers[i - 1]))
+                self.weights['W' + str(i+1)] = np.random.randn(layers[i], layers[i - 1]) *
+                                               np.sqrt(2 / (layers[i - 1]))
             self.weights['b' + str(i+1)] = np.zeros((layers[i], 1))
