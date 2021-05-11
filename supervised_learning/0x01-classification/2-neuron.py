d2640126569e778b62e1944e26df9521ab5e4260
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-05-12 01:00:30 +0200

    Update 2-neuron.py

--- supervised_learning/0x01-classification/2-neuron.py
+++ supervised_learning/0x01-classification/2-neuron.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python3
 
 import numpy as np
+"""
+Define a single neuron performing binary classification
+"""
+
 
 class Neuron:
-    
+    """
+    Class that define a single neuron performing binary classification
+    """
     def __init__(self, nx):
+        """
+        Class constructor
+        """
         if type(nx) is not int:
             raise TypeError("nx must be an integer")
         if nx < 1:
@@ -15,15 +24,29 @@ class Neuron:
         
     @property
     def W(self):
+        """
+        getter function
+        """
         return self.__W
+
     @property
     def b(self):
+        """
+        getter function
+        """
         return self.__b
+
     @property
     def A(self):
+        """
+        getter function
+        """
         return self.__A
     
     def forward_prop(self, X):
+        """
+        Calculates the forward propagation of the neuron
+        """
         r = np.dot(self.__W, X) + self.__b
         self.__A = 1 / (1 + np.exp(-r))
         return self.__A
