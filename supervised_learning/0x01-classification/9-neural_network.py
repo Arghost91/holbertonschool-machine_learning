bf453a0d8573e9137a4ba5950e66700289c0f698
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-05-12 01:27:14 +0200

    Update 9-neural_network.py

--- supervised_learning/0x01-classification/9-neural_network.py
+++ supervised_learning/0x01-classification/9-neural_network.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
 
 import numpy as np
+"""
+Define a neural network with one hidden layer performing binary classification
+"""
+
 
 class NeuralNetwork:
-    
+    """
+    Class that define a neural network with one hidden layer
+    performing binary classification
+    """
     def __init__(self, nx, nodes):
+        """
+        Class constructor
+        """
         if type(nx) is not int:
             raise TypeError("nx must be an integer")
         if nx < 1:
@@ -15,21 +25,45 @@ class NeuralNetwork:
         self.__W2 = np.random.normal(size=(1, nodes))
         self.__b2 = 0
         self.__A2 = 0
+
     @property
     def W1(self):
+        """
+        getter function
+        """
         return self.__W1
+
     @property
     def b1(self):
+        """
+        getter function
+        """
         return self.__b1
+
     @property
     def A1(self):
+        """
+        getter function
+        """
         return self.__A1
+
     @property
     def W2(self):
+        """
+        getter function
+        """
         return self.__W2
+
     @property
     def b2(self):
+        """
+        getter function
+        """
         return self.__b2
+
     @property
     def A2(self):
+        """
+        getter function
+        """
         return self.__A2
