b7604f19205348764e4d197bf48d3baf705c0e07
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-05-12 01:33:09 +0200

    Update 10-neural_network.py

--- supervised_learning/0x01-classification/10-neural_network.py
+++ supervised_learning/0x01-classification/10-neural_network.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
 
 import numpy as np
+"""
+Define a neural network with one hidden layer performing binary classification
+"""
+
 
 class NeuralNetwork:
-    
+    """
+    Class that define a neural network with one hidden layer
+    performing binary classification
+    """
     def __init__(self, nx, nodes):
+        """
+        Class constructor
+        """
         if type(nx) is not int:
             raise TypeError("nx must be an integer")
         if nx < 1:
@@ -15,26 +25,53 @@ class NeuralNetwork:
         self.__W2 = np.random.normal(size=(1, nodes))
         self.__b2 = 0
         self.__A2 = 0
+
     @property
     def W1(self):
+        """
+        getter function
+        """
         return self.__W1
+
     @property
     def b1(self):
+        """
+        getter function
+        """
         return self.__b1
+
     @property
     def A1(self):
+        """
+        getter function
+        """
         return self.__A1
+
     @property
     def W2(self):
+        """
+        getter function
+        """
         return self.__W2
+
     @property
     def b2(self):
+        """
+        getter function
+        """
         return self.__b2
+
     @property
     def A2(self):
+        """
+        getter function
+        """
         return self.__A2
-      
+
     def forward_prop(self, X):
+        """
+        Calculates the forward propagation of the neural network
+        """
         r1 = np.dot(self.__W1, X) + self.__b1
         self.__A1 = 1 / (1 + np.exp(-r1))
         r2 = np.dot(self.__W2, self.__A1) + self.__b2
