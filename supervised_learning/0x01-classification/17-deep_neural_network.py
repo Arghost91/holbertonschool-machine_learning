e7cfd11dc7ba85fe2aa0ddf3a7e04e2c0f87dca5
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-05-12 02:14:51 +0200

    Update 17-deep_neural_network.py

--- supervised_learning/0x01-classification/17-deep_neural_network.py
+++ supervised_learning/0x01-classification/17-deep_neural_network.py
@@ -24,12 +24,17 @@ class DeepNeuralNetwork:
         self.__cache = {}
         self.__weights = {}
         for i in range(self.L):
+            W = 'W' + str(i+1)
+            layers_1 = (layers[i - 1])
             if layers[i] < 0:
                 raise TypeError("layers must be a list of positive integers")
             if i == 0:
-                self.weights['W' + str(i+1)] = np.random.randn(layers[i], nx) * np.sqrt(2 / nx)
+                self.weights[W] = np.random.randn(layers[i],
+                                                  nx) * np.sqrt(2 / nx)
             else:
-                self.weights['W' + str(i+1)] = np.random.randn(layers[i], layers[i - 1]) * np.sqrt(2 / (layers[i - 1]))
+                sq2 = np.sqrt(2 / (layers_1))
+                self.weights[W] = np.random.randn(layers[i],
+                                                  layers_1) * sq2
             self.weights['b' + str(i+1)] = np.zeros((layers[i], 1))
 
     @property
