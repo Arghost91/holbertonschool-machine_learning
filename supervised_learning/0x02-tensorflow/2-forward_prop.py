4e703234946c62048580f427454f723297f9ae24
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-05-14 15:55:17 +0200

    Create 2-forward_prop.py

---
+++ supervised_learning/0x02-tensorflow/2-forward_prop.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Function that creates the forward propagation graph for the neural network
+"""
+
+
+import tensorflow as tf
+
+create_layer = __import__('1-create_layer').create_layer
+
+def forward_prop(x, layer_sizes=[], activations=[]):
+    """
+    * x is the placeholder for the input data
+    * layer_sizes is a list containing the number of nodes in each layer of the network
+    * activations is a list containing the activation functions for each layer of the network
+    * Returns: the prediction of the network in tensor form
+    """
+    for i in range(len(layer_sizes)):
+        forw = create_layer(x, layer_sizes[i], activations=[i])
+    return forw
