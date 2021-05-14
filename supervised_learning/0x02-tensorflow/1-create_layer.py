3a4a8d11346de6d917379df1c2b8db6d16247727
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-05-14 14:40:57 +0200

    Update 1-create_layer.py

--- supervised_learning/0x02-tensorflow/1-create_layer.py
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -9,10 +9,10 @@ def create_layer(prev, n, activation):
     """
     Function that return the tensor output of the layer
     """
-    in = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    initial = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
     layer = tf.layers.Dense(units=n,
                             activation=activation,
-                            kernel_initializer=in,
+                            kernel_initializer=initial,
                             name='layer'
                            )
     return layer(prev)
