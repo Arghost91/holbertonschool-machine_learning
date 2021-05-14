74d23e662b650b0cd8f7de64dbb746f4d89492d4
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-05-15 00:38:53 +0200

    Update 6-train.py

--- supervised_learning/0x02-tensorflow/6-train.py
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -52,7 +52,7 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha, i
             if i < iterations:
                 sess.run(
                     train_op, 
-                    feed_dict={X_train, y: Y_train}
+                    feed_dict={x: X_train, y: Y_train}
                 )
 
         return saver.save(
