275b4524188f487e1b26ea7f938a494147a25aec
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-05-15 00:51:58 +0200

    Update 3-calculate_accuracy.py

--- supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
+++ supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
@@ -4,6 +4,7 @@ Function that calculates the accuracy of a prediction
 """
 import tensorflow as tf
 
+
 def calculate_accuracy(y, y_pred):
     """
     * y is a placeholder for the labels of the input data
