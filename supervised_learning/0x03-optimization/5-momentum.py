eb59d8363db96d26463221634ad36e6dfbae04fb
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-05-18 19:20:51 +0200

    Create 5-momentum.py

---
+++ supervised_learning/0x03-optimization/5-momentum.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Function that updates a variable using the gradient descent with momentum
+"""
+
+
+import numpy as np
+
+def update_variables_momentum(alpha, beta1, var, grad, v):
+    """
+    * alpha is the learning rate
+    * beta1 is the momentum weight
+    * var is a numpy.ndarray containing the variable to be updated
+    * grad is a numpy.ndarray containing the gradient of var
+    * v is the previous first moment of var
+    * Returns: the updated variable and the new moment, respectively
+    """
+    Vd = beta * Vd + (1 - beta) * grad
+    var -= alpha * var
+    return var
