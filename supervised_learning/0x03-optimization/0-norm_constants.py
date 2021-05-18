1f441082ae0b7c238b85511df0ded8c9a09a7b93
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-05-18 13:03:32 +0200

    Create 0-norm_constants.py

---
+++ supervised_learning/0x03-optimization/0-norm_constants.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Calculates the normalization (standardization) constants of a matrix
+"""
+def normalization_constants(X):
+    """
+    Function that the mean and standard deviation of each feature, respectively
+    """
+    return X.mean(), X.std()
