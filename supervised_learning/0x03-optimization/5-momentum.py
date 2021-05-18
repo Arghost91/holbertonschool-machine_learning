bb2287e9b410e50fc8bf599064fa87cca2abc55d
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-05-18 19:23:11 +0200

    Update 5-momentum.py

--- supervised_learning/0x03-optimization/5-momentum.py
+++ supervised_learning/0x03-optimization/5-momentum.py
@@ -15,6 +15,6 @@ def update_variables_momentum(alpha, beta1, var, grad, v):
     * v is the previous first moment of var
     * Returns: the updated variable and the new moment, respectively
     """
-    Vd = beta * Vd + (1 - beta) * grad
+    Vd = beta1 * Vd + (1 - beta1) * grad
     var -= alpha * var
     return var
