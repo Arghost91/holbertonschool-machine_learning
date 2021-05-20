0109649ed94a89da40a74243665028db14ff3db2
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-05-20 19:42:03 +0200

    Create 7-RMSProp.py

---
+++ supervised_learning/0x03-optimization/7-RMSProp.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Function that updates a variable using the RMSProp optimization algorithm
+"""
+import tensorflow as tf
+
+
+def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
+    """
+    * alpha is the learning rate
+    * beta2 is the RMSProp weight
+    * epsilon is a small number to avoid division by zero
+    * var is a numpy.ndarray containing the variable to be updated
+    * grad is a numpy.ndarray containing the gradient of var
+    * s is the previous second moment of var
+    * Returns: the updated variable and the new moment, respectively
+    """
+    S_d = (beta2 * S_d) + ((1 - beta2) * grad ** 2) 
+    W = W - (alpha * (grad / (epsilon + (S_d ** (1 / 2))))
+    return W, S_d
