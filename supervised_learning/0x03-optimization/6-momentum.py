4f06c1619fe0ac36af63d03bd9bd461965b4da16
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-05-20 18:23:56 +0200

    Update 6-momentum.py

--- supervised_learning/0x03-optimization/6-momentum.py
+++ supervised_learning/0x03-optimization/6-momentum.py
@@ -7,7 +7,7 @@ import tensorflow as tf
 
 
 def create_momentum_op(loss, alpha, beta1):
-     """
+    """
     * loss is the loss of the network
     * alpha is the learning rate
     * beta1 is the momentum weight
