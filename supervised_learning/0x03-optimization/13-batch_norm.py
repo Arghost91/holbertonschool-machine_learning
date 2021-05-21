3740d80a1fcdc406dd803892b445485313b21b98
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-05-21 02:37:56 +0200

    Create 13-batch_norm.py

---
+++ supervised_learning/0x03-optimization/13-batch_norm.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Function that normalizes an unactivated output of
+a neural network using batch normalization
+"""
+
+
+def batch_norm(Z, gamma, beta, epsilon):
+    """
+    * Z is a numpy.ndarray of shape (m, n) that should be normalized
+    * m is the number of data points
+    * n is the number of features in Z
+    * gamma is a numpy.ndarray of shape (1, n) containing the scales used for batch normalization
+    * beta is a numpy.ndarray of shape (1, n) containing the offsets used for batch normalization
+    * epsilon is a small number used to avoid division by zero
+    * Returns: the normalized Z matrix
+    """
+    mean = np.mean(Z, axis=0)
+    var = np.var(Z, axis=0)
+    Z_norm = (Z * miu) / (((var ** 2) + epsilon) ** (1 / 2))
+    Z_mat = gamma * Z_norm + beta
+    return Z_mat
+    
