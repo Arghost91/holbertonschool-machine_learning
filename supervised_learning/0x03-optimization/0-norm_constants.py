337b3d673e1fc1647b7eed0f106c80a6e6e43b05
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-05-21 04:41:01 +0200

    Update 0-norm_constants.py

--- supervised_learning/0x03-optimization/0-norm_constants.py
+++ supervised_learning/0x03-optimization/0-norm_constants.py
@@ -2,10 +2,14 @@
 """
 Calculates the normalization (standardization) constants of a matrix
 """
+import numpy as np
 
 
 def normalization_constants(X):
     """
-    Function that the mean and standard deviation of each feature, respectively
+    * X is the numpy.ndarray of shape (m, nx) to normalize
+        * m is the number of data points
+        * nx is the number of features
+    * Returns: the mean and standard deviation of each feature, respectively
     """
-    return X.mean(axis=0), X.std(axis=0)
+    return np.mean(X, axis=0), np.std(X, axis=0)
