3d608e09d885e56a9576ce5e769d4d49fcbe84e2
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-05-21 04:45:40 +0200

    Update 2-shuffle_data.py

--- supervised_learning/0x03-optimization/2-shuffle_data.py
+++ supervised_learning/0x03-optimization/2-shuffle_data.py
@@ -15,6 +15,6 @@ def shuffle_data(X, Y):
         * ny is the number of features in Y
     * Returns: the shuffled X and Y matrices
     """
-    X_shuffled = X[np.random.permutation(len(X))]
-    Y_shuffled = Y[np.random.permutation(len(Y))]
+    X_shuffled = X[np.random.permutation(X.shape[0])]
+    Y_shuffled = Y[np.random.permutation(Y.shape[0])]
     return X_shuffled, Y_shuffled
