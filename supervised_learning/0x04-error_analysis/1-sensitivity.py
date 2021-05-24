e98af8b2e2431ba3a026687c903edf7fe2d23b79
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-05-25 01:54:11 +0200

    Update 1-sensitivity.py

--- supervised_learning/0x04-error_analysis/1-sensitivity.py
+++ supervised_learning/0x04-error_analysis/1-sensitivity.py
@@ -7,10 +7,12 @@ import numpy as np
 
 def sensitivity(confusion):
     """
-    * confusion is a confusion numpy.ndarray of shape (classes, classes) where row
-    indices represent the correct labels and column indices represent the predicted labels
+    * confusion is a confusion numpy.ndarray of shape (classes, classes)
+    where row indices represent the correct labels and column indices
+    represent the predicted labels
         *classes is the number of classes
-    * Returns: a numpy.ndarray of shape (classes,) containing the sensitivity of each class
+    * Returns: a numpy.ndarray of shape (classes,) containing the sensitivity
+    of each class
     """
     TP = np.diagonal(confusion)
     P = np.sum(confusion, axis=1)
