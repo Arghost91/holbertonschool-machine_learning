57ec0aae43dc45dcde7e12b5244f05a37ab4a0a1
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-05-25 01:18:24 +0200

    Create 3-specificity.py

---
+++ supervised_learning/0x04-error_analysis/3-specificity.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Function that calculates the specificity for each class in a confusion matrix
+"""
+import numpy as np
+
+
+def specificity(confusion):
+    """
+    * confusion is a confusion numpy.ndarray of shape (classes, classes) where
+    row indices represent the correct labels and column indices represent the predicted labels
+        * classes is the number of classes
+    * Returns: a numpy.ndarray of shape (classes,) containing the specificity of each class
+    """
+    FP = confusion.sum(axis=0) - np.diagonal(confusion)
+    FN = confusion.sum(axis=1) - np.diagonal(confusion)
+    TP = np.diagonal(confusion)
+    TN = confusion.sum() - (FP + FN + TP)
+    TNR = TN / (TN + FP)
+    return TNR
