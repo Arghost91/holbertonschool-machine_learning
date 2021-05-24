ceb4ac031bb39a4a8cc05ccfda641a12bf0001f9
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-05-25 01:57:48 +0200

    Update 4-f1_score.py

--- supervised_learning/0x04-error_analysis/4-f1_score.py
+++ supervised_learning/0x04-error_analysis/4-f1_score.py
@@ -7,10 +7,12 @@ import numpy as np
 
 def f1_score(confusion):
     """
-    * confusion is a confusion numpy.ndarray of shape (classes, classes) where row
-    indices represent the correct labels and column indices represent the predicted labels
+    * confusion is a confusion numpy.ndarray of shape (classes, classes)
+    where row indices represent the correct labels and column indices
+    represent the predicted labels
         * classes is the number of classes
-    * Returns: a numpy.ndarray of shape (classes,) containing the F1 score of each class
+    * Returns: a numpy.ndarray of shape (classes,) containing the F1 score
+    of each class
     """
     TP = np.diagonal(confusion)
     FP = confusion.sum(axis=0) - np.diagonal(confusion)
