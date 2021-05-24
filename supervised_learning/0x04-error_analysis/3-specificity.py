0d0dab609e88408cb3b9e92d4ede8db6114a5e0f
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-05-25 01:56:42 +0200

    Update 3-specificity.py

--- supervised_learning/0x04-error_analysis/3-specificity.py
+++ supervised_learning/0x04-error_analysis/3-specificity.py
@@ -8,9 +8,11 @@ import numpy as np
 def specificity(confusion):
     """
     * confusion is a confusion numpy.ndarray of shape (classes, classes) where
-    row indices represent the correct labels and column indices represent the predicted labels
+    row indices represent the correct labels and column indices represent the
+    predicted labels
         * classes is the number of classes
-    * Returns: a numpy.ndarray of shape (classes,) containing the specificity of each class
+    * Returns: a numpy.ndarray of shape (classes,) containing the specificity
+    of each class
     """
     FP = confusion.sum(axis=0) - np.diagonal(confusion)
     FN = confusion.sum(axis=1) - np.diagonal(confusion)
