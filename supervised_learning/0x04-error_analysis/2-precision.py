68aeee166487ad2af0aae8a3f54fdea8c738f310
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-05-25 01:55:01 +0200

    Update 2-precision.py

--- supervised_learning/0x04-error_analysis/2-precision.py
+++ supervised_learning/0x04-error_analysis/2-precision.py
@@ -8,9 +8,11 @@ import numpy as np
 def precision(confusion):
     """
     * confusion is a confusion numpy.ndarray of shape (classes, classes) where
-    row indices represent the correct labels and column indices represent the predicted labels
+    row indices represent the correct labels and column indices represent the
+    predicted labels
         * classes is the number of classes
-    * Returns: a numpy.ndarray of shape (classes,) containing the precision of each class
+    * Returns: a numpy.ndarray of shape (classes,) containing the precision of
+    each class
     """
     TP = np.diagonal(confusion)
     FP = np.sum(confusion, axis=0) - TP
