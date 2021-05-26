7d96214d8c5d0563999258bcbadf8bc2175190b9
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-05-26 19:28:30 +0200

    Update 5-dropout_gradient_descent.py

--- supervised_learning/0x05-regularization/5-dropout_gradient_descent.py
+++ supervised_learning/0x05-regularization/5-dropout_gradient_descent.py
@@ -27,8 +27,9 @@ def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
     weights_2 = weights.copy()
     dr = cache["A" + str(L)] - Y
     for i in range(L, 0, -1):
-        A_prev = cache['A' + str(i-1)]
+        A_prev = cache["A" + str(i-1)]
         W = weights["W" + str(i)]
+        b = weights["b" + str(i)]
         dW = (1 / m) * np.dot(dr, A_prev.T) 
         db = (1 / m) * np.sum(dr, axis=1, keepdims=True)
         weights["W" + str(i)] -= alpha * dW
