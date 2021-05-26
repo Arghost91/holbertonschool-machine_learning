0c6d8d0e55ce3d902a1c5b8e9af608a4c5f16a2c
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-05-26 18:09:19 +0200

    Update 4-dropout_forward_prop.py

--- supervised_learning/0x05-regularization/4-dropout_forward_prop.py
+++ supervised_learning/0x05-regularization/4-dropout_forward_prop.py
@@ -33,7 +33,8 @@ def dropout_forward_prop(X, weights, L, keep_prob):
         else:
             t = (np.exp(r) - np.exp(-r)) / (np.exp(r) + np.exp(-r))
             d = np.random.rand(t.shape[0], t.shape[1])
-            d = int(d < keep_prob)
+            d = d < keep_prob
+            d.astype(int)
             t = np.multiply(t, d)
             t /= keep_prob 
             cache["D" + str(i+1)] = d
