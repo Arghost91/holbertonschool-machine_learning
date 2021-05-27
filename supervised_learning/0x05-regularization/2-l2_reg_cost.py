85e0d3645e855b8be99b09041a65d75573304ecf
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-05-27 13:30:15 +0200

    Update 2-l2_reg_cost.py

--- supervised_learning/0x05-regularization/2-l2_reg_cost.py
+++ supervised_learning/0x05-regularization/2-l2_reg_cost.py
@@ -7,7 +7,9 @@ import tensorflow as tf
 
 def l2_reg_cost(cost):
     """
-    * cost is a tensor containing the cost of the network without L2 regularization
-    * Returns: a tensor containing the cost of the network accounting for L2 regularization
+    * cost is a tensor containing the cost of
+    the network without L2 regularization
+    * Returns: a tensor containing the cost of
+    the network accounting for L2 regularization
     """
     return cost + tf.losses.get_regularization_losses()
