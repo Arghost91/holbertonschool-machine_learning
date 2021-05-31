6c722636905eda22e6ad5c8502191a610b2a7d15
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-05-31 21:53:02 +0200

    Create 0-sequential.py

---
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Function that builds a neural network with the Keras library
+"""
+import tensorflow.keras as k
+
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    """
+    * nx is the number of input features to the network
+    * layers is a list containing the number of nodes in each layer of the network
+    * activations is a list containing the activation functions used for each layer of the network
+    * lambtha is the L2 regularization parameter
+    * keep_prob is the probability that a node will be kept for dropout
+    * You are not allowed to use the Input class
+    * Returns: the keras model
+    """
+    model = k.Sequential()
+    k_regularizer = k.regularizers.l2(lamptha)
+    for i in range(len(layers)):
+        model.add(k.layers.Dense(layers[i], input_dim=nx, activation=activations[i],
+                                kernel_regularizer=k_regularizer))
+        if i < (len(layers) - 1):
+            model.add(k.layers.Dropout(1 - keep_prob))
+    return model
