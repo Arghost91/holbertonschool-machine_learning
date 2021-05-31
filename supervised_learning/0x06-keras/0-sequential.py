a7af48dc55bd4866fbf2b28cb582d100012e2fbe
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-05-31 21:55:58 +0200

    Update 0-sequential.py

--- supervised_learning/0x06-keras/0-sequential.py
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -8,8 +8,10 @@ import tensorflow.keras as k
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """
     * nx is the number of input features to the network
-    * layers is a list containing the number of nodes in each layer of the network
-    * activations is a list containing the activation functions used for each layer of the network
+    * layers is a list containing the number of nodes in each
+    layer of the network
+    * activations is a list containing the activation functions
+    used for each layer of the network
     * lambtha is the L2 regularization parameter
     * keep_prob is the probability that a node will be kept for dropout
     * You are not allowed to use the Input class
@@ -18,8 +20,9 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     model = k.Sequential()
     k_regularizer = k.regularizers.l2(lambtha)
     for i in range(len(layers)):
-        model.add(k.layers.Dense(layers[i], input_dim=nx, activation=activations[i],
-                                kernel_regularizer=k_regularizer))
+        model.add(k.layers.Dense(layers[i], input_dim=nx,
+                                 activation=activations[i],
+                                 kernel_regularizer=k_regularizer))
         if i < (len(layers) - 1):
             model.add(k.layers.Dropout(1 - keep_prob))
     return model
