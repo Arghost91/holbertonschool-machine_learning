ef4c7eb5e1a6edf38d5cf5cb05f720e3705f8129
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-06-01 17:05:25 +0200

    Update 1-input.py

--- supervised_learning/0x06-keras/1-input.py
+++ supervised_learning/0x06-keras/1-input.py
@@ -1,4 +1,3 @@
-  
 #!/usr/bin/env python3
 """
 Function that builds a neural network with the Keras library
@@ -9,8 +8,10 @@ import tensorflow.keras as k
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """
     * nx is the number of input features to the network
-    * layers is a list containing the number of nodes in each layer of the network
-    * activations is a list containing the activation functions used for each layer of the network
+    * layers is a list containing the number of nodes in each
+    layer of the network
+    * activations is a list containing the activation functions used
+    for each layer of the network
     * lambtha is the L2 regularization parameter
     * keep_prob is the probability that a node will be kept for dropout
     * You are not allowed to use the Sequential class
