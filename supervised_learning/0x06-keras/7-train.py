a78692ce9e1c341fe1618d69b7e4f9e490ab3977
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-06-01 20:15:19 +0200

    Update 7-train.py

--- supervised_learning/0x06-keras/7-train.py
+++ supervised_learning/0x06-keras/7-train.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python3
 """
 Update the function to also train the model with learning rate decay
@@ -6,13 +5,15 @@ Update the function to also train the model with learning rate decay
 import tensorflow.keras as k
 
 
-def train_model(network, data, labels, batch_size, epochs, validation_data=None,
-                early_stopping=False, patience=0, learning_rate_decay=False,
-                alpha=0.1, decay_rate=1, verbose=True, shuffle=False):
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, early_stopping=False, patience=0,
+                learning_rate_decay=False, alpha=0.1, decay_rate=1,
+                verbose=True, shuffle=False):
     """
     * learning_rate_decay is a boolean that indicates whether learning rate
     decay should be used
-        * learning rate decay should only be performed if validation_data exists
+        * learning rate decay should only be performed if validation_data
+        exists
         * the decay should be performed using inverse time decay
         * the learning rate should decay in a stepwise fashion after each epoch
         * each time the learning rate updates, Keras should print a message
@@ -21,7 +22,7 @@ def train_model(network, data, labels, batch_size, epochs, validation_data=None,
     """
     def learning_rate_decay(epochs):
         return alpha / (1 + (decay_rate * epochs))
-    
+
     callbacks = []
     if validation_data:
         callbacks.append(k.callbacks.LearningRateScheduler(learning_rate_decay,
