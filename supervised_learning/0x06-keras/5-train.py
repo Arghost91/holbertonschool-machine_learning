a09fe056f43a24ec2acdc47ee8b7acd241569f96
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-06-01 18:41:25 +0200

    Create 5-train.py

---
+++ supervised_learning/0x06-keras/5-train.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Function to also analyze validaiton data
+"""
+import tensorflow.keras as k
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, verbose=True, shuffle=False):
+    """
+    * validation_data is the data to validate the
+    model with, if not None
+    """
+    train = network.fit(data, labels, batch_size=batch_size, epochs=epochs,
+                        validation_data=validation_data, verbose=verbose,
+                        shuffle=shuffle)
