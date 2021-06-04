031f07574f28df193f4d081dca9c513644b48f53
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-06-04 02:07:00 +0200

    Update 6-pool.py

--- math/0x04-convolutions_and_pooling/6-pool.py
+++ math/0x04-convolutions_and_pooling/6-pool.py
@@ -7,12 +7,14 @@ import numpy as np
 
 def pool(images, kernel_shape, stride, mode='max'):
     """
-    * images is a numpy.ndarray with shape (m, h, w, c) containing multiple images
+    * images is a numpy.ndarray with shape (m, h, w, c)
+    containing multiple images
         * m is the number of images
         * h is the height in pixels of the images
         * w is the width in pixels of the images
         * c is the number of channels in the image
-    * kernel_shape is a tuple of (kh, kw) containing the kernel shape for the pooling
+    * kernel_shape is a tuple of (kh, kw) containing the
+    kernel shape for the pooling
         * kh is the height of the kernel
         * kw is the width of the kernel
     * stride is a tuple of (sh, sw)
@@ -41,7 +43,7 @@ def pool(images, kernel_shape, stride, mode='max'):
     for x in range(output_width):
         for y in range(output_height):
             image_ = images[:, (sh * y):(sh * y) + height_ker,
-                             (sw * x):(sw * x) + width_ker]
+                            (sw * x):(sw * x) + width_ker]
             if mode == 'max':
                 output[:, y, x] = np.max(image_, axis=(1, 2))
             else:
