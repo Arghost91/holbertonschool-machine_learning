cbebb7682e38ba8a8e7976a8c4ee57e0e2e12601
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-06-08 00:42:15 +0200

    Update 0-conv_forward.py

--- supervised_learning/0x07-cnn/0-conv_forward.py
+++ supervised_learning/0x07-cnn/0-conv_forward.py
@@ -37,14 +37,14 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
         ph = 0
         pw = 0
 
+    out_pad = np.pad(A_prev, ((0, 0), (ph, ph), (pw, pw) (0, 0)), mode='constant', constant_values=(0, 0))
     out_h = (h_prev - kh + (2 * ph)) // (sh + 1)
     out_w = (w_prev - kw + (2 * pw)) // (sw + 1)
     out_conv = np.zeros((m, out_h, out_w, c_new))
-    out_pad = np.pad(A_prev, ((0, 0), (ph, ph), (pw, pw) (0, 0)), mode='constant', constant_values=(0, 0))
 
     for z in range(c_new):
         for x in range(out_h):
             for y in range(out_w):
-                output[:, y, x, z] = (W[:, :, :, z] * out_pad)[
-                    :, (sh * y):(sh * y) + kw, (sw * x):(sw * x) + kw].sum(axis=(1, 2, 3))
+                output[:, y, x, z] = activation((W[:, :, :, z] * out_pad)[
+                    :, (sh * y):(sh * y) + kw, (sw * x):(sw * x) + kw].sum(axis=(1, 2, 3))) + b
     return output
