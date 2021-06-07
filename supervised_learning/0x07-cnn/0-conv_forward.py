6f1a2fe9ef4970465fc26c78baa3871163934b48
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-06-08 01:06:31 +0200

    Update 0-conv_forward.py

--- supervised_learning/0x07-cnn/0-conv_forward.py
+++ supervised_learning/0x07-cnn/0-conv_forward.py
@@ -45,6 +45,6 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
     for z in range(c_new):
         for x in range(out_h):
             for y in range(out_w):
-                output[:, y, x, z] = activation((W[:, :, :, z] * out_pad)[
+                output[:, y, x, z] = activation((W[:, :, :, z] * out_pad[
                     :, (sh * y):(sh * y) + kw, (sw * x):(sw * x) + kw].sum(axis=(1, 2, 3))) + b
     return output
