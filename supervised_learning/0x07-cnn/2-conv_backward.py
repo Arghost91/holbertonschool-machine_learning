0903e0c24a79c68a08c59e7403030a5c8b548e05
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-06-09 19:08:51 +0200

    Update 2-conv_backward.py

--- supervised_learning/0x07-cnn/2-conv_backward.py
+++ supervised_learning/0x07-cnn/2-conv_backward.py
@@ -56,8 +56,8 @@ def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
             for z in range(w_new):
                 for i in range(c_new):
                     dA_prev[x, (sh * y):(sh * y) + kh, (sw * z):(sw * z) + kw, :]
-                    += dZ[x, y, z, i] * W[:, :, :, i]
+                    =+ dZ[x, y, z, i] * W[:, :, :, i]
 
-                    dW[:, :, :, i] += out_pad[x, (sh * y):(sh * y) + kh,
+                    dW[:, :, :, i] =+ out_pad[x, (sh * y):(sh * y) + kh,
                                               (sw * z):(sw * z) + kw, :] * dZ[x, y, z, i]
     return dA_prev, dW, db
