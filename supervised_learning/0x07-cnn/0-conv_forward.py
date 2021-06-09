c0af49615b1b1ac26260b012cba929316a12bf12
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-06-09 14:50:37 +0200

    Update 0-conv_forward.py

--- supervised_learning/0x07-cnn/0-conv_forward.py
+++ supervised_learning/0x07-cnn/0-conv_forward.py
@@ -1,64 +1,48 @@
 #!/usr/bin/env python3
 """
-performs forward propagation over a convolutional layer of a neural network
+Performs forward propagation over a convolutional layer of a neural network
 """
 import numpy as np
 
 
 def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
     """
-    *A_prev is a numpy.ndarray of shape (m, h_prev, w_prev, c_prev)
+    a function that performs forward propagation over a CNN
+    :param A_prev: numpy.ndarray of shape (m, h_prev, w_prev, c_prev)
     containing the output of the previous layer
-        m is the number of examples
-        h_prev is the height of the previous layer
-        w_prev is the width of the previous layer
-        c_prev is the number of channels in the previous layer
-    *W is a numpy.ndarray of shape (kh, kw, c_prev, c_new)
-    containing the kernels for the convolution
-        kh is the filter height
-        kw is the filter width
-        c_prev is the number of channels in the previous layer
-        c_new is the number of channels in the output
-    *b is a numpy.ndarray of shape (1, 1, 1, c_new) containing the
-    biases applied to the convolution
-    *activation is an activation function applied to the convolution
-    *padding is a string that is either same or valid, indicating
-    the type of padding used
-    *stride is a tuple of (sh, sw) containing the strides for the convolution
-        sh is the stride for the height
-        sw is the stride for the width
+    :param W: numpy.ndarray of shape (kh, kw, c_prev, c_new) containing the
+    kernels for the convolution
+    :param b: numpy.ndarray of shape (1, 1, 1, c_new) containing the biases
+    applied to the convolution
+    :param activation: is an activation function applied to the convolution
+    :param padding:  is a string that is either same or valid, indicating the
+    type of padding used
+    :param stride: is a tuple of (sh, sw) containing the strides for the
+    convolution
+    :return: the output of the convolutional layer
     """
-
-    kh, kw, c_prev, c_new = W.shape
     m, h_prev, w_prev, c_prev = A_prev.shape
-
+    kh, kw, c_prev, c_new = W.shape
     sh, sw = stride
-    if padding == 'valid':
-        output_h = int(((h_prev - kh) / sh) + 1)
-        output_w = int(((w_prev - kw) / sw) + 1)
-        image_padded = np.copy(A_prev)
 
-    elif padding == 'same':
+    pad_h, pad_w = (0, 0)
+    if padding == "same":
+        pad_h = int(np.ceil((((h_prev - 1) * sh + kh - h_prev) / 2)))
+        pad_w = int(np.ceil((((w_prev - 1) * sw + kw - w_prev) / 2)))
 
-        p_h = int((((h_prev - 1) * sh + kh - h_prev) / 2) + (kh % 2 == 0))
-        p_w = int((((w_prev - 1) * sw + kw - w_prev) / 2) + (kw % 2 == 0))
+    padded = np.pad(A_prev, ((0, 0), (pad_h, pad_h), (pad_w, pad_w), (0, 0)),
+                    mode="constant", constant_values=(0, 0))
 
-        """ output_h = h and output_w = w"""
+    conv_h = (h_prev + 2 * pad_h - kh) // sh + 1
+    conv_w = (w_prev + 2 * pad_w - kw) // sw + 1
+    convolved = np.zeros((m, conv_h, conv_w, c_new))
 
-        output_h = int(((h_prev - kh + (2 * p_h)) / sh) + 1)
-        output_w = int(((w_prev - kw + (2 * p_w)) / sw) + 1)
-
-        image_padded = np.pad(A_prev, ((0, 0), (p_h, p_h),
-                              (p_w, p_w), (0, 0)), 'constant')
-
-    output = np.zeros((m, output_h, output_w, c_new))
-
-    for x in range(output_w):
-        for y in range(output_h):
+    # In this notation row refers to height and col to width
+    for row in range(conv_h):
+        for col in range(conv_w):
             for ch in range(c_new):
-                output[:, y, x, ch] = (W[:, :, :, ch] *
-                                       image_padded[:, (sh * y): (sh * y) +
-                                       kh, (sw * x):  (sw * x) + kw]).sum(
-                                        axis=(1, 2, 3))
-
-    return activation(output + b)
+                slice_A = padded[:, row * sh:row * sh + kh, col * sw:col * sw
+                                 + kw]
+                slice_A_sum = np.sum(slice_A * W[:, :, :, ch], axis=(1, 2, 3))
+                convolved[:, row, col, ch] = slice_A_sum
+    return activation(convolved + b)
