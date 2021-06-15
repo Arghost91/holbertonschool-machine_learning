006fcb9c96908a5eb356c27ad7d25d7352089150
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-06-15 13:54:57 +0200

    Update 0-inception_block.py

--- supervised_learning/0x08-deep_cnns/0-inception_block.py
+++ supervised_learning/0x08-deep_cnns/0-inception_block.py
@@ -33,7 +33,8 @@ def inception_block(A_prev, filters):
                               activation=activation, kernel_initializer=init)(A_prev)
     conv_5 = K.layers.Conv2D(filters=F5, kernel_size=(5, 5), padding='same',
                              activation=activation, kernel_initializer=init)(conv_5R)
-    pool = K.layers.MaxPooling2D(pool_size=(3, 3), strides=(1, 1))(A_prev)
+    pool = K.layers.MaxPooling2D(pool_size=(3, 3), strides=(1, 1),
+                                padding='same')(A_prev)
     conv_PP = K.layers.Conv2D(filters=FPP, kernel_size=(1, 1), padding='same',
                               activation=activation, kernel_initializer=init)(pool)
     concatenate = K.layers.concatenate([conv_1, conv_3, conv_5, conv_PP], axis=3)
