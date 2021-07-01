001e85a7e84c66fe2e7aef0133f5253ce4e890d3
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-07-02 01:58:39 +0200

    Update 2-yolo.py

--- supervised_learning/0x0A-object_detection/2-yolo.py
+++ supervised_learning/0x0A-object_detection/2-yolo.py
@@ -109,9 +109,9 @@ class Yolo:
         for i in range(len(boxes)):
             box_scores.append(box_confidences[i] * box_class_probs[i])
             classes = np.argmax(box_scores[i], axis=3)
-            class_scores = np.max(box_scores[i], axis=3)
+            box_class_scores.append(np.max(box_scores[i], axis=3))
             filt = class_scores[i] >= self.class_t
-        for j, k in zip(class_scores, filt):
+        for j, k in zip(box_class_scores, filt):
             scores += j[k]
         for l, m in zip(boxes, filt):
             boxes += l[m]
