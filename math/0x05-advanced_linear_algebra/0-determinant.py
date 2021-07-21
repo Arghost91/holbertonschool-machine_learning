b8191d526c76c541028e966d343ca8c36d70a982
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-07-22 01:43:56 +0200

    Update 0-determinant.py

--- math/0x05-advanced_linear_algebra/0-determinant.py
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -23,7 +23,7 @@ def determinant(matrix):
         diag2 = matrix[0][1] * matrix[1][0]
         return diag1 - diag2
     elif len(matrix) == 3:
-        diag1 = matrix[0][0]  *  (matrix[1][1] * matrix[2][2] - matrix[1][2] * matrix[2][1])
-        diag2 = matrix[0][1]  *  (matrix[0][1] * matrix[2][2] - matrix[1][2] * matrix[0][2])
-        diag3 = matrix[0][2]  *  (matrix[0][1] * matrix[1][2] - matrix[2][2] * matrix[0][2])
+        diag1 = matrix[0][0]  *  ((matrix[1][1] * matrix[2][2]) - (matrix[1][2] * matrix[2][1]))
+        diag2 = matrix[0][1]  *  ((matrix[0][1] * matrix[2][2]) - (matrix[1][2] * matrix[0][2]))
+        diag3 = matrix[0][2]  *  ((matrix[0][1] * matrix[1][2]) - (matrix[1][1] * matrix[0][2]))
         return diag1 - diag2 + diag3
