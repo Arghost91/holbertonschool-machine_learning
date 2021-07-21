244c814245c91595ab548e92ba8cc12ab13539d6
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-07-22 00:50:46 +0200

    Create 0-determinant.py

---
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+
+"""
+
+
+def determinant(matrix):
+    if type(matrix) is not a list:
+        raise TypeError("matrix must be a list of lists")
+    if len(matrix) != len(matrix[0]):
+        raise ValueError("matrix must be a square matrix")
+    else:
+        if len(matrix) = 1:
+            diag1 = matrix[0,0] * matrix[1,1]
+            diag2 = matrix[0,1] * matrix[1,0]
+            det = diag1 - diag2
+        elif len(matrix) = 2:
+            diag1 = matrix[0,0]  *  (matrix[1,1] * matrix[2,2] - matrix[1,2] * matrix[2,1])
+            diag2 = matrix[0,1]  *  (matrix[0,1] * matrix[2,2] - matrix[1,2] * matrix[0,3])
+            diag3 = matrix[0,2]  *  (matrix[0,1] * matrix[1,2] - matrix[2,2] * matrix[0,2])
+            det = diag1 - diag2 + diag3
+        elif len(matrix) = 0:
+            det = 1
+    return det
