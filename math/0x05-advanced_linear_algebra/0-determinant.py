007682c76c1d699b0f4f4f28ef582db6f77671fa
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-07-22 01:20:37 +0200

    Update 0-determinant.py

--- math/0x05-advanced_linear_algebra/0-determinant.py
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -12,7 +12,9 @@ def determinant(matrix):
             if len(matrix) != len(matrix[0]):
                 raise ValueError("matrix must be a square matrix")
     else:
-        if len(matrix) == 1:
+        if len(matrix) == 0:
+            det = 1
+        elif len(matrix) == 1:
             diag1 = matrix[0][0] * matrix[1][1]
             diag2 = matrix[0][1] * matrix[1][0]
             det = diag1 - diag2
@@ -21,6 +23,4 @@ def determinant(matrix):
             diag2 = matrix[0][1]  *  (matrix[0][1] * matrix[2][2] - matrix[1][2] * matrix[0][3])
             diag3 = matrix[0][2]  *  (matrix[0][1] * matrix[1][2] - matrix[2][2] * matrix[0][2])
             det = diag1 - diag2 + diag3
-        elif len(matrix) == 0:
-            det = 1
     return det
