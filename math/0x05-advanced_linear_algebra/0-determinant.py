f0239ffb820b72febc32bda6c3ba9ed0223275df
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-07-22 02:11:05 +0200

    Update 0-determinant.py

--- math/0x05-advanced_linear_algebra/0-determinant.py
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -12,9 +12,6 @@ def determinant(matrix):
     for i in range(len(matrix)):
         if len(matrix) != len(matrix[i]):
             raise ValueError("matrix must be a square matrix")
-    for j in matrix:
-        if type(j) is not list:
-            raise TypeError("matrix must be a list of lists")
     if len(matrix) == 1:
         return matrix[0][0]
     if len(matrix) == 2:
