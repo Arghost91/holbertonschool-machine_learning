0d3e9d5a4ff0bc28291e3b35635ccf7957f58359
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-07-23 02:15:47 +0200

    Update 1-minor.py

--- math/0x05-advanced_linear_algebra/1-minor.py
+++ math/0x05-advanced_linear_algebra/1-minor.py
@@ -1,79 +1,76 @@
 #!/usr/bin/env python3
 """
-Calculates the minor of a matrix
+Function that calculates the minor matrix of a matrix
 """
 
 
 def determinant(matrix):
     """
-    a funciton that calculates the determinant of a matrix
-    :param matrix: a list of lists whose determinant should be calculated
-    :return: the determinant of matrix
+    * matrix is a list of lists whose determinant should be calculated
+    * If matrix is not a list of lists, raise a TypeError with the
+    message matrix must be a list of lists
+    * If matrix is not square, raise a ValueError with the message
+    matrix must be a square matrix
+    * The list [[]] represents a 0x0 matrix
+    * Returns: the determinant of matrix
     """
     if type(matrix) is not list:
         raise TypeError("matrix must be a list of lists")
-
     if matrix == [[]]:
         return 1
-
     for i in range(len(matrix)):
         if len(matrix) != len(matrix[i]):
             raise ValueError("matrix must be a square matrix")
         if type(matrix[i]) is not list or not len(matrix[i]):
             raise TypeError("matrix must be a list of lists")
-
-    if len(matrix) == 1 and len(matrix[0]) == 1:
+    if len(matrix) == 1:
         return matrix[0][0]
-
-    if len(matrix) == 2 and len(matrix[0]) == 2:
-        return (matrix[0][0] * matrix[1][1]) - (matrix[0][1] * matrix[1][0])
-
-    first_row = matrix[0]
-    determ = 0
+    if len(matrix) == 2:
+        diag1 = matrix[0][0] * matrix[1][1]
+        diag2 = matrix[0][1] * matrix[1][0]
+        return diag1 - diag2
+    row = matrix[0]
+    det = 0
     cof = 1
     for i in range(len(matrix[0])):
-        next_matrix = [x[:] for x in matrix]
-        del next_matrix[0]
-        for mat in next_matrix:
-            del mat[i]
-        determ += first_row[i] * determinant(next_matrix) * cof
+        mat = [l[:] for l in matrix]
+        del mat[0]
+        for m in mat:
+            del m[i]
+        det += row[i] * determinant(mat) * cof
         cof = cof * -1
-
-    return determ
+    return det
 
 
 def minor(matrix):
     """
-    a function that calculates the minor of a matrix
-    :param matrix: matrix is a list of lists whose minor matrix should be
-    calculated
-    :return: the minor matrix of a matrix
+    * matrix is a list of lists whose minor matrix should be calculated
+    * If matrix is not a list of lists, raise a TypeError with
+    the message matrix must be a list of lists
+    * If matrix is not square or is empty, raise a ValueError with
+    the message matrix must be a non-empty square matrix
+    * Returns: the minor matrix of matrix
     """
     if type(matrix) is not list or not len(matrix):
         raise TypeError("matrix must be a list of lists")
-
     if matrix == [[]]:
-        raise ValueError("matrix must be a non-empty square matrix")
-
+        raise ValueError("matrix must be a square matrix")
     for i in range(len(matrix)):
         if len(matrix) != len(matrix[i]):
-            raise ValueError("matrix must be a non-empty square matrix")
+            raise ValueError("matrix must be a square matrix")
         if type(matrix[i]) is not list or not len(matrix[i]):
             raise TypeError("matrix must be a list of lists")
-
     if len(matrix) == 1:
         return [[1]]
-
-    list_minor = []
+    minor = []
     for i in range(len(matrix)):
         inner = []
-        for j in range(len(matrix[0])):
-            next_matrix = [x[:] for x in matrix]
-            del next_matrix[i]
-            for mat in next_matrix:
-                del mat[j]
-            determ = determinant(next_matrix)
-            inner.append(determ)
-        list_minor.append(inner)
-
-    return list_minor
+        for j in range(len(matrix)):
+            mat = [l[:] for l in matrix]
+            del mat[i]
+            for m in mat:
+                del m[j]
+            det = determinant(mat)
+            inner.append(det)
+        minor.append(inner)
+    return minor
