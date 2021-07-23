50981149964e81ee3dd363c044393af4c40f6ad8
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-07-23 03:04:03 +0200

    Update 2-cofactor.py

--- math/0x05-advanced_linear_algebra/2-cofactor.py
+++ math/0x05-advanced_linear_algebra/2-cofactor.py
@@ -12,19 +12,24 @@ def determinant(matrix):
     """
     if type(matrix) is not list:
         raise TypeError("matrix must be a list of lists")
+
     if matrix == [[]]:
         return 1
+
     for i in range(len(matrix)):
         if len(matrix) != len(matrix[i]):
             raise ValueError("matrix must be a square matrix")
         if type(matrix[i]) is not list or not len(matrix[i]):
             raise TypeError("matrix must be a list of lists")
+
     if len(matrix) == 1 and len(matrix[0]) == 1:
         return matrix[0][0]
+
     if len(matrix) == 2 and len(matrix[0]) == 2:
         diag1 = matrix[0][0] * matrix[1][1]
         diag2 = matrix[0][1] * matrix[1][0]
         return diag1 - diag2
+
     row = matrix[0]
     det = 0
     cofact = 1
@@ -47,15 +52,19 @@ def cofactor(matrix):
     """
     if type(matrix) is not list or not len(matrix):
         raise TypeError("matrix must be a list of lists")
+
     if matrix == [[]]:
         raise ValueError("matrix must be a non-empty square matrix")
+
     for i in range(len(matrix)):
         if len(matrix) != len(matrix[i]):
             raise ValueError("matrix must be a non-empty square matrix")
         if type(matrix[i]) is not list or not len(matrix[i]):
             raise TypeError("matrix must be a list of lists")
+
     if len(matrix) == 1:
         return [[1]]
+
     minor = []
     for i in range(len(matrix)):
         inner = []
