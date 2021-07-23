201db3ce3c03ee16b5f48e43ab2a8ff3acf334aa
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-07-23 03:09:51 +0200

    Update 2-cofactor.py

--- math/0x05-advanced_linear_algebra/2-cofactor.py
+++ math/0x05-advanced_linear_algebra/2-cofactor.py
@@ -44,41 +44,40 @@ def determinant(matrix):
 
 def cofactor(matrix):
     """
-    a function that calculates the minor of a matrix
-    :param matrix: matrix is a list of lists whose minor matrix should be
-    calculated
-    :return: the minor matrix of a matrix
+    * matrix is a list of lists whose cofactor matrix should
+    be calculated
+    * If matrix is not a list of lists, raise a TypeError with
+    the message matrix must be a list of lists
+    * If matrix is not square or is empty, raise a ValueError
+    with the message matrix must be a non-empty square matrix
+    * Returns: the cofactor matrix of matrix
     """
     if type(matrix) is not list or not len(matrix):
         raise TypeError("matrix must be a list of lists")
-
     if matrix == [[]]:
-        raise ValueError("matrix must be a non-empty square matrix")
-
+        raise ValueError("matrix must be a square matrix")
     for i in range(len(matrix)):
         if len(matrix) != len(matrix[i]):
-            raise ValueError("matrix must be a non-empty square matrix")
+            raise ValueError("matrix must be a square matrix")
         if type(matrix[i]) is not list or not len(matrix[i]):
             raise TypeError("matrix must be a list of lists")
-
     if len(matrix) == 1:
         return [[1]]
-
-    list_minor = []
+    minor = []
     for i in range(len(matrix)):
         inner = []
-        if i % 2 == 0:
-            cof = 1
+        if i % 2 != 0:
+            cofact = -1
         else:
-            cof = -1
+            cofact = 1
         for j in range(len(matrix[0])):
-            next_matrix = [x[:] for x in matrix]
-            del next_matrix[i]
-            for mat in next_matrix:
-                del mat[j]
-            determ = determinant(next_matrix) * cof
-            inner.append(determ)
-            cof = cof * -1
-        list_minor.append(inner)
+            mat = [l[:] for l in matrix]
+            del mat[i]
+            for m in mat:
+                del m[j]
+            det = determinant(mat) * cofact
+            inner.append(det)
+            cofact = cofact * (-1)
+        minor.append(inner)
+    return minor
 
-    return list_minor
