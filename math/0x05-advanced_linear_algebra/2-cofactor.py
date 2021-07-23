64490455a04af595edfe592baf34fc08d757330c
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-07-23 03:12:26 +0200

    Update 2-cofactor.py

--- math/0x05-advanced_linear_algebra/2-cofactor.py
+++ math/0x05-advanced_linear_algebra/2-cofactor.py
@@ -6,40 +6,33 @@ Calculates the minor of a matrix
 
 def determinant(matrix):
     """
-    a funciton that calculates the determinant of a matrix
-    :param matrix: a list of lists whose determinant should be calculated
-    :return: the determinant of matrix
+    * matrix is a list of lists whose determinant should be calculated
+    * Returns: the determinant of matrix
     """
     if type(matrix) is not list:
         raise TypeError("matrix must be a list of lists")
-
     if matrix == [[]]:
         return 1
-
     for i in range(len(matrix)):
         if len(matrix) != len(matrix[i]):
             raise ValueError("matrix must be a square matrix")
         if type(matrix[i]) is not list or not len(matrix[i]):
             raise TypeError("matrix must be a list of lists")
-
     if len(matrix) == 1 and len(matrix[0]) == 1:
         return matrix[0][0]
-
     if len(matrix) == 2 and len(matrix[0]) == 2:
         return (matrix[0][0] * matrix[1][1]) - (matrix[0][1] * matrix[1][0])
-
-    first_row = matrix[0]
-    determ = 0
-    cof = 1
+    row = matrix[0]
+    det = 0
+    cofact = 1
     for i in range(len(matrix[0])):
-        next_matrix = [x[:] for x in matrix]
-        del next_matrix[0]
-        for mat in next_matrix:
-            del mat[i]
-        determ += first_row[i] * determinant(next_matrix) * cof
-        cof = cof * -1
-
-    return determ
+        mat = [l[:] for l in matrix]
+        del mat[0]
+        for m in mat:
+            del m[i]
+        det += row[i] * determinant(mat) * cofact
+        cofact = cofact * -1
+    return det
 
 
 def cofactor(matrix):
