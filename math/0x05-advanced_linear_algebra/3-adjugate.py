788a328fa25fc356d11d150db5fd3f1d39875eb7
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-07-23 03:40:57 +0200

    Update 3-adjugate.py

--- math/0x05-advanced_linear_algebra/3-adjugate.py
+++ math/0x05-advanced_linear_algebra/3-adjugate.py
@@ -37,42 +37,44 @@ def determinant(matrix):
 
 def cofactor(matrix):
     """
-    * matrix is a list of lists whose cofactor matrix should
-    be calculated
-    * If matrix is not a list of lists, raise a TypeError with
-    the message matrix must be a list of lists
-    * If matrix is not square or is empty, raise a ValueError
-    with the message matrix must be a non-empty square matrix
-    * Returns: the cofactor matrix of matrix
+    a function that calculates the cofactor of a matrix
+    :param matrix: matrix is a list of lists whose minor matrix should be
+    calculated
+    :return: the cofactor matrix of a matrix
     """
     if type(matrix) is not list or not len(matrix):
         raise TypeError("matrix must be a list of lists")
+
     if matrix == [[]]:
         raise ValueError("matrix must be a non-empty square matrix")
+
     for i in range(len(matrix)):
         if len(matrix) != len(matrix[i]):
             raise ValueError("matrix must be a non-empty square matrix")
         if type(matrix[i]) is not list or not len(matrix[i]):
             raise TypeError("matrix must be a list of lists")
+
     if len(matrix) == 1:
         return [[1]]
-    minor = []
+
+    list_minor = []
     for i in range(len(matrix)):
         inner = []
-        if i % 2 != 0:
-            cofact = -1
+        if i % 2 == 0:
+            cof = 1
         else:
-            cofact = 1
+            cof = -1
         for j in range(len(matrix[0])):
-            mat = [l[:] for l in matrix]
-            del mat[i]
-            for m in mat:
-                del m[j]
-            det = determinant(mat) * cofact
-            inner.append(det)
-            cofact = cofact * (-1)
-        minor.append(inner)
-    return minor
+            next_matrix = [x[:] for x in matrix]
+            del next_matrix[i]
+            for mat in next_matrix:
+                del mat[j]
+            determ = determinant(next_matrix) * cof
+            inner.append(determ)
+            cof = cof * -1
+        list_minor.append(inner)
+
+    return list_minor
 
 
 def adjugate(matrix):
