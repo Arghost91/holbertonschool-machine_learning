61593f49eb439b9af8cc654b331d35740d78a2e8
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-07-26 21:56:21 +0200

    Create multinormal.py

---
+++ math/0x06-multivariate_prob/multinormal.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Class that represents a Multivariate Normal distribution
+"""
+import numpy as np
+
+
+def __init__(self, data):
+    """
+    * data is a numpy.ndarray of shape (d, n) containing the data set:
+    * n is the number of data points
+    * d is the number of dimensions in each data point
+    * If data is not a 2D numpy.ndarray, raise a TypeError with the
+    message data must be a 2D numpy.ndarray
+    * If n is less than 2, raise a ValueError with the message data
+    must contain multiple data points
+    """
+    if type(data) is not np.ndarray:
+        raise TypeError("data must be a 2D numpy.ndarray")
+    if len(data.shape) != 2:
+        raise TypeError("data must be a 2D numpy.ndarray")
+    d, n = data.shape
+    if n < 2:
+        raise ValueError("X must contain multiple data points")
+    self.mean = np.mean(X, axis=0).reshape(d, 1)
+    sub_mean = X - self.mean
+    self.cov = np.dot(sub_mean.T, sub_mean) / (n - 1)
