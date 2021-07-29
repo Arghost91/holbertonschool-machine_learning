8caf6e59c394f814d1b23cdaeb14bb57eadb2472
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-07-29 03:11:36 +0200

    Create 3-entropy.py

---
+++ unsupervised_learning/0x00-dimensionality_reduction/3-entropy.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Function that calculates the Shannon entropy and P
+affinities relative to a data poin
+"""
+import numpy as np
+
+
+def HP(Di, beta):
+    """
+    * Di is a numpy.ndarray of shape (n - 1,) containing the pariwis
+    distances between a data point and all other points except itself
+        * n is the number of data points
+    * beta is a numpy.ndarray of shape (1,) containing the beta value
+    for the Gaussian distribution
+    * Returns: (Hi, Pi)
+        * Hi: the Shannon entropy of the points
+        * Pi: a numpy.ndarray of shape (n - 1,) containing the P affinities
+        of the points
+    """
+    Pi = np.exp(-Di * beta) / np.sum(-Di * beta)
+    Hi = - np.sum(Pi * np.log2(Pi))
+    return Hi, Pi
