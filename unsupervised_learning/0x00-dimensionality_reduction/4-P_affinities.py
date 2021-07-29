262a66b0f0deb63dbc52393a82e2caedf141b722
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-07-29 03:24:13 +0200

    Create 4-P_affinities.py

---
+++ unsupervised_learning/0x00-dimensionality_reduction/4-P_affinities.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Function that calculates the symmetric P affinities of a data set
+"""
+import numpy as np
+
+
+def P_affinities(X, tol=1e-5, perplexity=30.0):
+    """
+    * X is a numpy.ndarray of shape (n, d) containing the dataset to be transformed by t-SNE
+        * n is the number of data points
+        * d is the number of dimensions in each point
+    * perplexity is the perplexity that all Gaussian distributions should have
+    * tol is the maximum tolerance allowed (inclusive) for the difference in
+    Shannon entropy from perplexity for all Gaussian distributions
+    * You should use P_init = __import__('2-P_init').P_init and HP = __import__('3-entropy').HP
+    * Returns: P, a numpy.ndarray of shape (n, n) containing the symmetric P affinities
+    """
+    
