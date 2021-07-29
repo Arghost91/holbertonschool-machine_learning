d7a373c90658c2ed1692de4be8802a3266cc49f2
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-07-30 01:38:24 +0200

    Update 1-intersection.py

--- math/0x07-bayesian_prob/1-intersection.py
+++ math/0x07-bayesian_prob/1-intersection.py
@@ -6,29 +6,6 @@ this data with the various hypothetical probabilities
 import numpy as np
 
 
-def likelihood(x, n, P):
-    """
-    Function that calculates the likelihood of obtaining this data given
-    various hypothetical probabilities of developing severe side effects
-    """
-    if type(n) is not int or n <= 0:
-        raise ValueError("n must be a positive integer")
-    err = "x must be an integer that is greater than or equal to 0"
-    if type(x) is not int or x < 0:
-        raise ValueError(err)
-    if x > n:
-        raise ValueError("x cannot be greater than n")
-    if type(P) is not np.ndarray or len(P.shape) != 1:
-        raise TypeError("P must be a 1D numpy.ndarray")
-    for i in P:
-        if i < 0 or i > 1:
-            raise ValueError("All values in P must be in the range [0, 1]")
-    likelihood = np.math.factorial(n) / (np.math.factorial(x) *
-                                         np.math.factorial(n - x))
-    likelihood = likelihood * ((P ** x) * (1 - P) ** (n - x))
-    return likelihood
-
-
 def intersection(x, n, P, Pr):
     """
     * x is the number of patients that develop severe side effects
@@ -74,5 +51,8 @@ def intersection(x, n, P, Pr):
             raise ValueError("All values in Pr must be in the range [0, 1]")
     if not np.isclose(np.sum(Pr), 1):
         raise ValueError("Pr must sum to 1")
-    intersec = likelihood(x, n, P) * Pr
+    likelihood = np.math.factorial(n) / (np.math.factorial(x) *
+                                         np.math.factorial(n - x))
+    likelihood = likelihood * ((P ** x) * (1 - P) ** (n - x))
+    intersec = likelihood * Pr
     return intersec
