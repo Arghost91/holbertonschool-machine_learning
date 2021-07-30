f90b34ea5774e833c8f7cfc9fc249006a3ff4554
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-07-30 02:47:02 +0200

    Update 3-posterior.py

--- math/0x07-bayesian_prob/3-posterior.py
+++ math/0x07-bayesian_prob/3-posterior.py
@@ -128,20 +128,22 @@ def posterior(x, n, P, Pr):
     * P is a 1D numpy.ndarray containing the various hypothetical probabilities
     of developing severe side effects
     * Pr is a 1D numpy.ndarray containing the prior beliefs of P
-    * If n is not a positive integer, raise a ValueError with the message n must
-    be a positive integer
+    * If n is not a positive integer, raise a ValueError with the message n
+    must be a positive integer
     * If x is not an integer that is greater than or equal to 0, raise a
-    ValueError with the message x must be an integer that is greater than or equal to 0
+    ValueError with the message x must be an integer that is greater than or
+    equal to 0
     * If x is greater than n, raise a ValueError with the message x cannot be
     greater than n
-    * If P is not a 1D numpy.ndarray, raise a TypeError with the message P must be
-    a 1D numpy.ndarray
-    * If Pr is not a numpy.ndarray with the same shape as P, raise a TypeError with
-    the message Pr must be a numpy.ndarray with the same shape as P
-    * If any value in P or Pr is not in the range [0, 1], raise a ValueError with
-    the message All values in {P} must be in the range [0, 1] where {P} is the
-    incorrect variable
-    * If Pr does not sum to 1, raise a ValueError with the message Pr must sum to 1
+    * If P is not a 1D numpy.ndarray, raise a TypeError with the message P must
+    be a 1D numpy.ndarray
+    * If Pr is not a numpy.ndarray with the same shape as P, raise a TypeError
+    with the message Pr must be a numpy.ndarray with the same shape as P
+    * If any value in P or Pr is not in the range [0, 1], raise a ValueError
+    with the message All values in {P} must be in the range [0, 1] where {P}
+    is the incorrect variable
+    * If Pr does not sum to 1, raise a ValueError with the message Pr must
+    sum to 1
     * Returns: the posterior probability of each probability in P given x and n,
     respectively
     """
