b97915ae25945aaa1704902e63b3a6eef2bed32d
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-07-30 02:00:14 +0200

    Update 2-marginal.py

--- math/0x07-bayesian_prob/2-marginal.py
+++ math/0x07-bayesian_prob/2-marginal.py
@@ -115,24 +115,5 @@ def marginal(x, n, P, Pr):
     sum to 1
     * Returns: the marginal probability of obtaining x and n
     """
-    if type(n) is not int or n <= 0:
-        raise ValueError("n must be a positive integer")
-    err = "x must be an integer that is greater than or equal to 0"
-    if type(x) is not int or x < 0:
-        raise ValueError(err)
-    if x > n:
-        raise ValueError("x cannot be greater than n")
-    if type(P) is not np.ndarray or len(P.shape) != 1:
-        raise TypeError("P must be a 1D numpy.ndarray")
-    err_2 = "Pr must be a numpy.ndarray with the same shape as P"
-    if type(Pr) is not np.ndarray or Pr.shape != P.shape:
-        raise TypeError(err_2)
-    for i, j in zip(P, Pr):
-        if i < 0 or i > 1:
-            raise ValueError("All values in P must be in the range [0, 1]")
-        if j < 0 or j > 1:
-            raise ValueError("All values in Pr must be in the range [0, 1]")
-    if not np.isclose(np.sum(Pr), 1):
-        raise ValueError("Pr must sum to 1")
     margin = np.sum(intersection(x, n, P, Pr))
     return margin
