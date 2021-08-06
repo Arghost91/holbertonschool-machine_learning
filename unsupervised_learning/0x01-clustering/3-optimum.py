9fb4bb2c55533791aea0ea13248ee56f236efc3a
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-08-06 04:31:15 +0200

    Update 3-optimum.py

--- unsupervised_learning/0x01-clustering/3-optimum.py
+++ unsupervised_learning/0x01-clustering/3-optimum.py
@@ -11,13 +11,17 @@ variance = __import__('2-variance').variance
 def optimum_k(X, kmin=1, kmax=None, iterations=1000):
     """
     * X is a numpy.ndarray of shape (n, d) containing the data set
-    * kmin is a positive integer containing the minimum number of clusters to check for (inclusive)
-    * kmax is a positive integer containing the maximum number of clusters to check for (inclusive)
-    * iterations is a positive integer containing the maximum number of iterations for K-means
+    * kmin is a positive integer containing the minimum number of
+    clusters to check for (inclusive)
+    * kmax is a positive integer containing the maximum number of
+    clusters to check for (inclusive)
+    * iterations is a positive integer containing the maximum number
+    of iterations for K-means
     * This function should analyze at least 2 different cluster sizes
     * Returns: results, d_vars, or None, None on failure
     * results is a list containing the outputs of K-means for each cluster size
-    * d_vars is a list containing the difference in variance from the smallest cluster size for each cluster size
+    * d_vars is a list containing the difference in variance from the
+    smallest cluster size for each cluster size
     """
     if type(X) is not np.ndarray or len(X.shape) != 2:
         return None, None
