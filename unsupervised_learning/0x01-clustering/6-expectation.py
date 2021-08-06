a24c560bcb11f9dcc0d36c75d44e70c1716232e0
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-08-06 05:40:45 +0200

    Update 6-expectation.py

--- unsupervised_learning/0x01-clustering/6-expectation.py
+++ unsupervised_learning/0x01-clustering/6-expectation.py
@@ -9,11 +9,15 @@ pdf = __import__('5-pdf').pdf
 def expectation(X, pi, m, S):
     """
     * X is a numpy.ndarray of shape (n, d) containing the data set
-    * pi is a numpy.ndarray of shape (k,) containing the priors for each cluster
-    * m is a numpy.ndarray of shape (k, d) containing the centroid means for each cluster
-    * S is a numpy.ndarray of shape (k, d, d) containing the covariance matrices for each cluster
+    * pi is a numpy.ndarray of shape (k,) containing the priors for
+    each cluster
+    * m is a numpy.ndarray of shape (k, d) containing the centroid
+    means for each cluster
+    * S is a numpy.ndarray of shape (k, d, d) containing the
+    covariance matrices for each cluster
     * Returns: g, l, or None, None on failure
-        * g is a numpy.ndarray of shape (k, n) containing the posterior probabilities for each data point in each cluster
+        * g is a numpy.ndarray of shape (k, n) containing the posterior
+        probabilities for each data point in each cluster
         * l is the total log likelihood
     """
     if type(X) is not np.ndarray or len(X.shape) != 2:
