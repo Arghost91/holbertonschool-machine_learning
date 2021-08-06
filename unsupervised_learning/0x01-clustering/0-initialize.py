9256862bda4a4a27a64ce82dda75b801567f2487
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-08-06 02:05:10 +0200

    Create 0-initialize.py

---
+++ unsupervised_learning/0x01-clustering/0-initialize.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Function that initializes cluster centroids for K-means
+"""
+import numpy as np
+
+
+def initialize(X, k):
+    """
+    * X is a numpy.ndarray of shape (n, d) containing the dataset that will be used for K-means clustering
+    * n is the number of data points
+    * d is the number of dimensions for each data point
+    * k is a positive integer containing the number of clusters
+    * The cluster centroids should be initialized with a multivariate uniform distribution along each dimension in d:
+    * The minimum values for the distribution should be the minimum values of X along each dimension in d
+    * The maximum values for the distribution should be the maximum values of X along each dimension in d
+    * You should use numpy.random.uniform exactly once
+    * Returns: a numpy.ndarray of shape (k, d) containing the initialized centroids for each cluster, or None on failure
+    """
+    if type(x) is not np.ndarray:
+        return None
+    if type(k) is not int or k <= 0:
+        return None
+    n, d = X.shape
+    clust = np.random.uniform(np.min(X, axis=0),
+                              np.max(X, axis=0),
+                              size=(k, d))
+    return clust
