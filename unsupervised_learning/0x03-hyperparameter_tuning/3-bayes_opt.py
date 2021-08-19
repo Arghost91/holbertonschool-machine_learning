019c8449942996a4b91bc0ace83e3128030b055d
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-08-19 22:47:24 +0200

    Update 3-bayes_opt.py

--- unsupervised_learning/0x03-hyperparameter_tuning/3-bayes_opt.py
+++ unsupervised_learning/0x03-hyperparameter_tuning/3-bayes_opt.py
@@ -17,16 +17,19 @@ def __init__(self, f, X_init, Y_init, bounds, ac_samples, l=1, sigma_f=1,
     * t is the number of initial samples
     * bounds is a tuple of (min, max) representing the bounds of the space in
     which to look for the optimal point
-    * ac_samples is the number of samples that should be analyzed during acquisition
+    * ac_samples is the number of samples that should be analyzed during
+    acquisition
     * l is the length parameter for the kernel
-    * sigma_f is the standard deviation given to the output of the black-box function
+    * sigma_f is the standard deviation given to the output of the black-box
+    function
     * xsi is the exploration-exploitation factor for acquisition
     * minimize is a bool determining whether optimization should be performed
     for minimization (True) or maximization (False)
     * Sets the following public instance attributes:
         * f: the black-box function
         * gp: an instance of the class GaussianProcess
-        * X_s: a numpy.ndarray of shape (ac_samples, 1) containing all acquisition
+        * X_s: a numpy.ndarray of shape (ac_samples, 1) containing all
+        acquisition
         sample points, evenly spaced between min and max
         * xsi: the exploration-exploitation factor
         * minimize: a bool for minimization versus maximization
