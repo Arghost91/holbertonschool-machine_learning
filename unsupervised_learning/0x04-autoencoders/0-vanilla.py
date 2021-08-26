51d00c96c36907d94c511c563831a3cc7db97c73
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-08-26 23:18:40 +0200

    Update 0-vanilla.py

--- unsupervised_learning/0x04-autoencoders/0-vanilla.py
+++ unsupervised_learning/0x04-autoencoders/0-vanilla.py
@@ -8,15 +8,19 @@ import tensorflow.keras as keras
 def autoencoder(input_dims, hidden_layers, latent_dims):
     """
     * input_dims is an integer containing the dimensions of the model input
-    * hidden_layers is a list containing the number of nodes for each hidden layer in the encoder, respectively
+    * hidden_layers is a list containing the number of nodes for each hidden
+    layer in the encoder, respectively
         * the hidden layers should be reversed for the decoder
-    * latent_dims is an integer containing the dimensions of the latent space representation
+    * latent_dims is an integer containing the dimensions of the latent space
+    representation
     * Returns: encoder, decoder, auto
         * encoder is the encoder model
         * decoder is the decoder model
         * auto is the full autoencoder model
-    * The autoencoder model should be compiled using adam optimization and binary cross-entropy loss
-    * All layers should use a relu activation except for the last layer in the decoder, which should use sigmoid
+    * The autoencoder model should be compiled using adam optimization and
+    binary cross-entropy loss
+    * All layers should use a relu activation except for the last layer in
+    the decoder, which should use sigmoid
     """
     num = len(hidden_layers)
     input_img = keras.Input(shape=(input_dims,))
