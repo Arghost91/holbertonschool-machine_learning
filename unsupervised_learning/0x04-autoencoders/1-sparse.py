7ba067e0cbc5da7fc2c67bd5f164586a71df6dee
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-08-27 00:05:01 +0200

    Update 1-sparse.py

--- unsupervised_learning/0x04-autoencoders/1-sparse.py
+++ unsupervised_learning/0x04-autoencoders/1-sparse.py
@@ -8,16 +8,21 @@ import tensorflow.keras as keras
 def autoencoder(input_dims, hidden_layers, latent_dims, lambtha):
     """
     * input_dims is an integer containing the dimensions of the model input
-    * hidden_layers is a list containing the number of nodes for each hidden layer in the encoder, respectively
+    * hidden_layers is a list containing the number of nodes for each hidden
+    layer in the encoder, respectively
         * the hidden layers should be reversed for the decoder
-    * latent_dims is an integer containing the dimensions of the latent space representation
-    * lambtha is the regularization parameter used for L1 regularization on the encoded output
+    * latent_dims is an integer containing the dimensions of the latent
+    space representation
+    * lambtha is the regularization parameter used for L1 regularization
+    on the encoded output
     * Returns: encoder, decoder, auto
         * encoded is the encoder model
         * decoder is the decoder model
         * auto is the sparse autoencoder model
-    * The sparse autoencoder model should be compiled using adam optimization and binary cross-entropy loss
-    * All layers should use a relu activation except for the last layer in the decoder, which should use sigmoid
+    * The sparse autoencoder model should be compiled using adam optimization
+    and binary cross-entropy loss
+    * All layers should use a relu activation except for the last layer in the
+    decoder, which should use sigmoid
     """
     num = len(hidden_layers)
     input_img = keras.Input(shape=(input_dims,))
