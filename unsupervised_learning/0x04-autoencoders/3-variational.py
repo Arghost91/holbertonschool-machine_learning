ee48cf3fa8fe8cfe1c35db61b1912374c1f905aa
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-08-27 04:30:12 +0200

    Update 3-variational.py

--- unsupervised_learning/0x04-autoencoders/3-variational.py
+++ unsupervised_learning/0x04-autoencoders/3-variational.py
@@ -8,14 +8,19 @@ import tensorflow.keras as keras
 def autoencoder(input_dims, hidden_layers, latent_dims):
     """
     * input_dims is an integer containing the dimensions of the model input
-    * hidden_layers is a list containing the number of nodes for each hidden layer in the encoder, respectively
+    * hidden_layers is a list containing the number of nodes for each hidden
+    layer in the encoder, respectively
         * the hidden layers should be reversed for the decoder
-    * latent_dims is an integer containing the dimensions of the latent space representation
+    * latent_dims is an integer containing the dimensions of the latent space
+    representation
     * Returns: encoder, decoder, auto
-        * encoder is the encoder model, which should output the latent representation, the mean, and the log variance, respectively
+        * encoder is the encoder model, which should output the latent
+        representation, the mean, and the log variance, respectively
         * decoder is the decoder model
     * auto is the full autoencoder model
-    * The autoencoder model should be compiled using adam optimization and binary cross-entropy loss
-    * All layers should use a relu activation except for the mean and log variance layers in the encoder, which should use None, and the last layer in the decoder, which should use sigmoid
+    * The autoencoder model should be compiled using adam optimization and
+    binary cross-entropy loss
+    * All layers should use a relu activation except for the mean and log variance
+    layers in the encoder, which should use None, and the last layer in the decoder,
+    which should use sigmoid
     """
-    
