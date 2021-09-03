41b68dee653c32236cc6f1fc92303e9c1a60a9d3
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-09-03 06:34:29 +0200

    Create 8-bi_rnn.py

---
+++ supervised_learning/0x0D-RNNs/8-bi_rnn.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+Function that performs forward propagation for a bidirectional RNN
+"""
+import numpy as np
+
+
+def bi_rnn(bi_cell, X, h_0, h_t):
+    """
+    * bi_cell is an instance of BidirectinalCell that will be used for the forward propagation
+    * X is the data to be used, given as a numpy.ndarray of shape (t, m, i)
+        * t is the maximum number of time steps
+        * m is the batch size
+        * i is the dimensionality of the data
+    * h_0 is the initial hidden state in the forward direction, given as a numpy.ndarray of shape (m, h)
+        * h is the dimensionality of the hidden state
+    * h_t is the initial hidden state in the backward direction, given as a numpy.ndarray of shape (m, h)
+    * Returns: H, Y
+        * H is a numpy.ndarray containing all of the concatenated hidden states
+        * Y is a numpy.ndarray containing all of the outputs
+    """
+    t, m, i = X.shape
+    h = h_t.shape[1]
+    h_pev, h_ant = np.zeros((t, m, h))
+    for i in range(t):
+        x_pev = X[i]
+        x_ant = X[-(i + 1)]
+        H_p = bi_cell.forward(h_0, x_pev)
+        H_a = bi_cell.forward(h_t, x_ant)
+        h_pev[i] = H_p
+        h_ant[-(step + 1)] = H_a
+    H = no.concatenate((h_pev, h_ant), axis=1)
+    Y = bi_cell.output[H]
+    return H, Y
