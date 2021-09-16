7511bf2d4b3fb89704fd96282ddd262a1ca49428
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-09-17 01:42:00 +0200

    Create 0-uni_bleu.py

---
+++ supervised_learning/0x10-nlp_metrics/0-uni_bleu.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+Function that calculates the n-gram BLEU score for a sentence
+"""
+import numpy as np
+
+
+def ngram_bleu(references, sentence, n):
+    """
+    * references is a list of reference translations
+        * each reference translation is a list of the words in the translation
+    * sentence is a list containing the model proposed sentence
+    * n is the size of the n-gram to use for evaluation
+    * Returns: the n-gram BLEU score
+    """
+    senten_lenght = len(sentence)
+    ref_len = []
+    words = {}
+    for transl in references:
+        ref_len.append(len(transl))
+        for word in transl:
+            if word in sentence and word not in words.keys():
+                words[word] = 1
+    s = len(sentence)
+    total = sum(words.values())
+    index = np.argmin([abs(len(i) - senten_lenght) for i in references])
+    best_match = len(references[index])
+
+    if senten_lenght > best_match:
+        BLEU = 1
+    else:
+        BLEU = np.exp(1 - float(best_match) / float(senten_lenght))
+    BLEU_score = BLEU * np.exp(np.log(total / senten_lenght))
+
+    return BLEU_score
