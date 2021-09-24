34da7ed22fcdb24c3ec39d7835971473b909dbac
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-09-24 02:45:23 +0200

    Update 2-rnn_decoder.py

--- supervised_learning/0x11-attention/2-rnn_decoder.py
+++ supervised_learning/0x11-attention/2-rnn_decoder.py
@@ -1,4 +1,3 @@
-   
 #!/usr/bin/env python3
 """
 Creates the class SelfAttention
@@ -14,13 +13,17 @@ Class RNNDecoder(tf.keras.layers.Layer):
     def __init__(self, vocab, embedding, units, batch):
         """
         * vocab is an integer representing the size of the output vocabulary
-        * embedding is an integer representing the dimensionality of the embedding vector
-        * units is an integer representing the number of hidden units in the RNN cell
+        * embedding is an integer representing the dimensionality of the
+        embedding vector
+        * units is an integer representing the number of hidden units in
+        the RNN cell
         * batch is an integer representing the batch size
         * Sets the following public instance attributes:
-            * embedding - a keras Embedding layer that converts words from the vocabulary into an embedding vector
+            * embedding - a keras Embedding layer that converts words from
+            the vocabulary into an embedding vector
             * gru - a keras GRU layer with units units
-                * Should return both the full sequence of outputs as well as the last hidden state
+                * Should return both the full sequence of outputs as well
+                as the last hidden state
                 * Recurrent weights should be initialized with glorot_uniform
             * F - a Dense layer with vocab units
         """
@@ -37,14 +40,19 @@ Class RNNDecoder(tf.keras.layers.Layer):
 
     def call(self, x, s_prev, hidden_states):
         """
-        * x is a tensor of shape (batch, 1) containing the previous word in the target sequence as an index of the target vocabulary
-        * s_prev is a tensor of shape (batch, units) containing the previous decoder hidden state
-        * hidden_states is a tensor of shape (batch, input_seq_len, units)containing the outputs of the encoder
+        * x is a tensor of shape (batch, 1) containing the previous word in
+        the target sequence as an index of the target vocabulary
+        * s_prev is a tensor of shape (batch, units) containing the previous
+        decoder hidden state
+        * hidden_states is a tensor of shape (batch, input_seq_len, units)
+        containing the outputs of the encoder
         * You should use SelfAttention = __import__('1-self_attention').SelfAttention
         * You should concatenate the context vector with x in that order
         * Returns: y, s
-            * y is a tensor of shape (batch, vocab) containing the output word as a one hot vector in the target vocabulary
-            * s is a tensor of shape (batch, units) containing the new decoder hidden state
+            * y is a tensor of shape (batch, vocab) containing the output word
+            as a one hot vector in the target vocabulary
+            * s is a tensor of shape (batch, units) containing the new decoder
+            hidden state
         """
         attention = SelfAttention(self.units)
         context, weights = self.attention(s_prev, hidden_states)
