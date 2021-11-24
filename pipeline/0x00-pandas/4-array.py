067898528c21995589085e590ea918b69f80b7e0
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-11-24 22:00:57 +0100

    Update 4-array.py

--- pipeline/0x00-pandas/4-array.py
+++ pipeline/0x00-pandas/4-array.py
@@ -6,5 +6,6 @@ from_file = __import__('2-from_file').from_file
 df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
 
 A = df[['High', 'Close']].tail(10)
+A = A.to_numpy()
 
 print(A)
