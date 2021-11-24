cd1edd7739460777c7c1656b07995f81f020faf6
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-11-25 00:54:51 +0100

    Update 11-concat.py

--- pipeline/0x00-pandas/11-concat.py
+++ pipeline/0x00-pandas/11-concat.py
@@ -7,7 +7,7 @@ df1 = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
 df2 = from_file('bitstampUSD_1-min_data_2012-01-01_to_2020-04-22.csv', ',')
 
 df2.sort_values(by='Timestamp', ascending=True, inplace=True)
-df2 = df2[df2['Timestamp'] <= 1417411920]
+df2 = df2.loc[:"1417411920"]
 
 df1 = df1.set_index('Timestamp')
 df2 = df2.set_index('Timestamp')
