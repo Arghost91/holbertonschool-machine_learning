97a3882cb0d677e3359c349fef467f3ee2c2a996
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-11-24 18:57:47 +0100

    Update 1-from_dictionary.py

--- pipeline/0x00-pandas/1-from_dictionary.py
+++ pipeline/0x00-pandas/1-from_dictionary.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 index = ['A', 'B', 'C', 'D']
 diction = {
-    "First": [0.0, 0.5, 1.0, 1.5]
+    "First": [0.0, 0.5, 1.0, 1.5],
     "Second": ['one', 'two', 'three', 'four']
 }
 df = pd.DataFrame(index, data=diction)
