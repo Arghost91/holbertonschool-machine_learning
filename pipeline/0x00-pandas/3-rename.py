4ae8a6c281c26191ee036a08d01068f377b5357f
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-11-24 19:35:51 +0100

    Update 3-rename.py

--- pipeline/0x00-pandas/3-rename.py
+++ pipeline/0x00-pandas/3-rename.py
@@ -5,7 +5,7 @@ from_file = __import__('2-from_file').from_file
 
 df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
 
-df = DataFrame.rename(columns={"Timestamp": "Datetime"})
+df = df.rename(columns={"Timestamp": "Datetime"})
 df['Datetime'] = pd.to_datetime(df['Datetime'], unit='s')
 df = df[['Datetime', 'Close']]
 
