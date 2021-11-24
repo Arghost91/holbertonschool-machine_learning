1c3df9429a83d5c224432b75de5ab40277c75a68
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-11-24 23:39:13 +0100

    Update 9-fill.py

--- pipeline/0x00-pandas/9-fill.py
+++ pipeline/0x00-pandas/9-fill.py
@@ -7,10 +7,10 @@ df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
 
 df.drop('Weighted_Price', inplace=True, axis=1)
 
-df = df.fillna({'High': df['Close'].shift(periods=1, fill_value=0),
+df = df.fillna({'Close': df['Close'].shift(periods=1, fill_value=0),
+                'High': df['Close'].shift(periods=1, fill_value=0),
                 'Low': df['Close'].shift(periods=1, fill_value=0),
-                'Open': df['Close'].shift(periods=1, fill_value=0),
-                'Close': df['Close'].shift(periods=1, fill_value=0)})
+                'Open': df['Close'].shift(periods=1, fill_value=0)})
 
 df['Volume_(BTC)'].fillna(value=0, inplace=True)
 df['Volume_(Currency)'].fillna(value=0, inplace=True)
