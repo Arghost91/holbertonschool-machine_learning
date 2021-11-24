6cd70bf46101ab0e2ac66d9dbe98309506ba78b9
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-11-24 22:16:58 +0100

    Update 6-flip_switch.py

--- pipeline/0x00-pandas/6-flip_switch.py
+++ pipeline/0x00-pandas/6-flip_switch.py
@@ -6,6 +6,6 @@ from_file = __import__('2-from_file').from_file
 df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
 
 df = df.transpose
-df = df.iloc[::-1]
+df = df.iloc[::-1, :]
 
 print(df.tail(8))
