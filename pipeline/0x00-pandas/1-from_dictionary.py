908a57e2fb3cbb9f48f590b6b63c5c6ca60d46d0
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-11-24 18:55:29 +0100

    Create 1-from_dictionary.py

---
+++ pipeline/0x00-pandas/1-from_dictionary.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Python script that created a pd.DataFrame from a dictionary
+"""
+import pandas as pd
+
+
+index = ['A', 'B', 'C', 'D']
+diction = {
+    "First": [0.0, 0.5, 1.0, 1.5]
+    "Second": ['one', 'two', 'three', 'four']
+}
+df = pd.DataFrame(index, data=diction)
