33f3365b85039056e1f9a444f1c5ce653e6a51f2
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-11-25 01:32:15 +0100

    Update 13-analyze.py

--- pipeline/0x00-pandas/13-analyze.py
+++ pipeline/0x00-pandas/13-analyze.py
@@ -5,6 +5,7 @@ from_file = __import__('2-from_file').from_file
 
 df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
 
-stats = df.describe(include=['Open', 'High', 'Volume_(BTC)', 'Volume_(Currency)', 'Weighted_Price'])
+df = df.drop(['Timestamp'], axis=1)
+stats = df.describe(include='all')
 
 print(stats)
