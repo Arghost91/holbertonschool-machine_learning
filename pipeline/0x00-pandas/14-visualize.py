67e89b9fac8fdd096fcdcb07e9496c97fcbded18
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-11-25 02:02:08 +0100

    Update 14-visualize.py

--- pipeline/0x00-pandas/14-visualize.py
+++ pipeline/0x00-pandas/14-visualize.py
@@ -10,6 +10,7 @@ df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
 df.rename(columns={'Timestamp': 'Date'}, inplace=True)
 df['Date'] = pd.to_datetime(df['Date'], unit='s')
 df = df.set_index('Date')
+df = df.drop(['Weighted_Price'], axis=1)
 df['Close'].fillna(method='ffill', inplace=True)
 df = df.fillna({'High': df['Close'].shift(periods=1, fill_value=0),
                 'Low': df['Close'].shift(periods=1, fill_value=0),
@@ -21,7 +22,6 @@ df = df[(df.index >= '2017-01-01')]
 df = df.resample('D').agg({'Open': 'first', 'High': 'max', 'Low': 'min',
                           'Close': 'last', 'Volume_(BTC)': 'sum',
                           'Volume_(Currency)': 'sum'})
-df = df.drop(['Weighted_Price'], axis=1)
 
 df.plot()
 plt.show()
