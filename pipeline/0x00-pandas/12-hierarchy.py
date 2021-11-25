89495a8fd3c258d971392df6b2ead25608a25c2a
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-11-25 01:15:56 +0100

    Update 12-hierarchy.py

--- pipeline/0x00-pandas/12-hierarchy.py
+++ pipeline/0x00-pandas/12-hierarchy.py
@@ -7,9 +7,8 @@ df1 = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
 df2 = from_file('bitstampUSD_1-min_data_2012-01-01_to_2020-04-22.csv', ',')
 
 df1.sort_values(by='Timestamp', ascending=True, inplace=True)
-df2.sort_values(by='Timestamp', ascending=True, inplace=True)
-
 df1.set_index('Timestamp', inplace=True)
+df2.sort_values(by='Timestamp', ascending=True, inplace=True)
 df2.set_index('Timestamp', inplace=True)
 
 df1 = df1.loc["1417411980":"1417417980"]
