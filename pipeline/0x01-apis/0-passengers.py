e6b712515d80971e1cf3b0c8ee597065a10288a3
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-11-29 22:41:58 +0100

    Create 0-passengers.py

---
+++ pipeline/0x01-apis/0-passengers.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Method that returns the list of ships that can hold a given number of passengers
+"""
+import requests
+
+
+def availableShips(passengerCount):
+    """
+    * Prototype: def availableShips(passengerCount):
+    * Don’t forget the pagination
+    * If no ship available, return an empty list.
+    """
+    url = "https://swapi-api.hbtn.io/api/starships/"
+    r = requests.get('url')
+    ships = []
+    while(r.status_code == 200):
+        json_result = r.json()["results"]
+        for ship in json_result:
+            passen = ship["passen"].replace(',', '')
+            if int(passen) >= passengerCount:
+                ships.append(ship["name"])
+            elif passen == 'unknown' or passen == 'n/a':
+                continue
+        url = json["next"]
+    return ships
