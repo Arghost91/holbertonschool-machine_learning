9af806bf1c34048698ea3b0986d1e35231139692
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-11-30 03:40:32 +0100

    Create 2-user_location.py

---
+++ pipeline/0x01-apis/2-user_location.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Script that prints the location of a specific user
+"""
+import requests
+import sys
+import time
+
+
+if __name__ == '__main__':
+    """
+    * The user is passed as first argument of the script with the full API
+    URL, example: ./2-user_location.py https://api.github.com/users/holbertonschool
+    * If the user doesn’t exist, print Not found
+    * If the status code is 403, print Reset in X min where X is the number
+    of minutes from now and the value of X-Ratelimit-Reset
+    """
+    url = sys.argv[1]
+    par = {"Accept: application/vnd.github.v3+json"}
+    r = requests.get(url, par=par)
+    r_req = r.status_code
+    if r_req == 200:
+        print(r.json()["location"])
+    elif r_req == 403:
+        ratelimit = int(r.headers["X-Ratelimit-Reset"])
+        time = int(time.time())
+        reset = (ratelimit - time) / 60
+        print("Reset in {} min".format(reset))
+    else:
+        print("Not Found")
