df5a491df99f8288763f1613964b7f3ebe07a36c
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-11-30 04:45:59 +0100

    Create 3-upcoming.py

---
+++ pipeline/0x01-apis/3-upcoming.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+Script that displays the upcoming launch with these information
+"""
+import requests
+
+
+if __name__ == '__main__':
+    r = requests.get("https://api.spacexdata.com/v4/launches/upcoming")
+    r_get = r.json()
+    for i in r_get:
+        dates = i["date_unix"]
+    next = r_get[dates.index(min(dates))]
+    name = next["name"]
+    date_local = next["date_local"]
+    rocket = next["rocket"]
+    rocket_url = "https://api.spacexdata.com/v4/rockets" + rocket
+    r_rocket = requests.get(rocket_url)
+    r_rocket_get = r_roquet.json()
+    rocket_name = r_rocket_get["name"]
+    launchpad = next["launchpad"]
+    launchpad_url = "https://api.spacexdata.com/v4/launchpads" + launchpad
+    r_launchpad = requests.get(launchpad_url)
+    r_launchpad_get = r_launchpad.json()
+    launchpad_name = r_launchpad_get["name"]
+    launchpad_locality = r_launchpad_get["locality"]
+print("{} ({}) {} - {} ({})".format(name,
+                                   date_local,
+                                   rocket_name,
+                                   launchpad_name,
+                                   launchpad_locality))
