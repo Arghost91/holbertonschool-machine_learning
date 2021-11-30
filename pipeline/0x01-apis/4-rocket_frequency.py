3e10b0cbdd4cbe7b9fb30b666b8fa7379e5bd636
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-11-30 05:45:19 +0100

    Update 4-rocket_frequency.py

--- pipeline/0x01-apis/4-rocket_frequency.py
+++ pipeline/0x01-apis/4-rocket_frequency.py
@@ -1,25 +1,33 @@
 #!/usr/bin/env python3
 """
-Script that displays the number of launches per rocket
+Displays the number of launches per rocket
 """
 import requests
 
 
 if __name__ == '__main__':
-    r = requests.get("https://api.spacexdata.com/v4/launches/")
+
     rockets = {}
-    r_get = r.json()
-    for launch in r_get:
-        rocket = launch["rocket"]
-        rocket_url = "https://api.spacexdata.com/v4/rockets/" + rocket
-        r_rocket = requests.get(rocket_url)
-        r_rocket_get = r_rocket.json()
-        rocket_name = r_rocket_get["name"]
-        if rocket in rockets.keys():
-            rockets[rocket] = rockets[rocket] + 1
+
+    url = 'https://api.spacexdata.com/v4/launches'
+    r = requests.get(url)
+    launches = r.json()
+
+    for launch in launches:
+        rocket_id = launch['rocket']
+        url_r = "https://api.spacexdata.com/v4/rockets/{}".\
+            format(rocket_id)
+        req_r = requests.get(url_r)
+        json_r = req_r.json()
+        rocket_name = json_r['name']
+
+        if rocket_name in rockets.keys():
+            rockets[rocket_name] += 1
         else:
-            rockets[rocket] = 1
-    sorting = sorted(rockets.items(), key=lambda x: x[0])
-    sorting = sorted(sorting, key=lambda x: x[1], reverse=True)
-    for i in sorting:
+            rockets[rocket_name] = 1
+
+    sort = sorted(rockets.items(), key=lambda x: x[0])
+    sort = sorted(sort, key=lambda x: x[1], reverse=True)
+
+    for i in sort:
         print("{}: {}".format(i[0], i[1]))
