9f58914dc9ea98d6d5345a9fbfa07d83189f7985
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-11-30 03:53:20 +0100

    Update 2-user_location.py

--- pipeline/0x01-apis/2-user_location.py
+++ pipeline/0x01-apis/2-user_location.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Prints the location of a specific Github user
+Script that prints the location of a specific user
 """
 import requests
 import sys
@@ -8,20 +8,21 @@ import time
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        exit()
-    url = sys.argv[1]
-    headers = {'Accept': 'application/vnd.github.v3+json'}
-    r = requests.get(url, headers=headers)
-
-    if r.status_code == 200:
-        print(r.json()['location'])
-
-    if r.status_code == 404:
-        print("Not found")
-
-    if r.status_code == 403:
-        rate_limit = int(r.headers['X-Ratelimit-Reset'])
-        now = int(time.time())
-        minutes = int((rate_limit - now) / 60)
-        print("Reset in {} min".format(minutes))
+    """
+    * The user is passed as first argument of the script with the full API
+    URL, example: ./2-user_location.py https://api.github.com/users/holbertonschool
+    * If the user doesn’t exist, print Not found
+    * If the status code is 403, print Reset in X min where X is the number
+    of minutes from now and the value of X-Ratelimit-Reset
+    """
+    r = requests.get(sys.argv[0])
+    r_req = r.status_code
+    if r_req == 200:
+        print(r.json()["location"])
+    elif r_req == 403:
+        ratelimit = int(r.headers["X-Ratelimit-Reset"])
+        time = int(time.time())
+        reset = (ratelimit - time) / 60
+        print("Reset in {} min".format(reset))
+    else:
+        print("Not Found")
