99974fb80070b0dff7fa49bf1d226c5582d196c5
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-11-30 03:49:03 +0100

    Update 2-user_location.py

--- pipeline/0x01-apis/2-user_location.py
+++ pipeline/0x01-apis/2-user_location.py
@@ -1,28 +1,20 @@
 #!/usr/bin/env python3
-"""
-Script that prints the location of a specific user
-"""
+"""Print location of a user"""
+
+
 import requests
 import sys
 import time
 
 
 if __name__ == '__main__':
-    """
-    * The user is passed as first argument of the script with the full API
-    URL, example: ./2-user_location.py https://api.github.com/users/holbertonschool
-    * If the user doesn’t exist, print Not found
-    * If the status code is 403, print Reset in X min where X is the number
-    of minutes from now and the value of X-Ratelimit-Reset
-    """
-    r = requests.get(sys.argv[1])
-    r_req = r.status_code
-    if r_req == 200:
-        print(r.json()["location"])
-    elif r_req == 403:
-        ratelimit = int(r.headers["X-Ratelimit-Reset"])
-        time = int(time.time())
-        reset = (ratelimit - time) / 60
-        print("Reset in {} min".format(reset))
+    data = requests.get(sys.argv[1])
+    response = data.status_code
+    if response == 200:
+        print(data.json()['location'])
+    elif response == 403:
+        reset = data.headers['X-Ratelimit-Reset']
+        reset = int(reset) - int(time.time())
+        print("Reset in {} min".format(int(reset / 60)))
     else:
-        print("Not Found")
+        print("Not found")
