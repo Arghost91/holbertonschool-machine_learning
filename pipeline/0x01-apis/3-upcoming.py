f3b39aba338a939944232e8efda7eecefa79ddf3
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-11-30 05:22:31 +0100

    Update 3-upcoming.py

--- pipeline/0x01-apis/3-upcoming.py
+++ pipeline/0x01-apis/3-upcoming.py
@@ -1,28 +1,31 @@
 #!/usr/bin/env python3
-"""script that displays the upcoming launch with these information"""
-import sys
-import requests as rq
-import time
+"""  task 3 """
+import requests
 
 
 if __name__ == '__main__':
-    url = "https://api.spacexdata.com/v4/launches/upcoming"
-    r = rq.get(url)
-    launches = sorted(r.json(), key=lambda i: i['date_unix'])
-    date_unix = launches[0]['date_unix']
-    for i in r.json():
-        if i['date_unix'] == date_unix:
-            launch_name = i['name']
-            date = i['date_local']
-            rocket_id = i['rocket']
-            launchpad_id = i['launchpad']
-            break
-    url = "https://api.spacexdata.com/v4/rockets/{}".format(rocket_id)
-    r = rq.get(url)
-    rocket_name = r.json()['name']
-    url = "https://api.spacexdata.com/v4/launchpads/{}".format(launchpad_id)
-    r = rq.get(url)
-    lpad_name = r.json()['name']
-    lpad_locality = r.json()['locality']
-    print("{} ({}) {} - {} ({})".format(launch_name, date, rocket_name,
-                                        lpad_name, lpad_locality))
+    """
+    give lunch info
+    """
+    ADRESS = "https://api.spacexdata.com/v4/launches/upcoming"
+    RES = requests.get(ADRESS)
+    data = RES.json()
+    data.sort(key=lambda json: json['date_unix'])
+    data = data[0]
+
+    F_NOM = data["name"]
+
+    F_tmp_LOC = data["date_local"]
+
+    ADRESS2 = "https://api.spacexdata.com/v4/rockets/" + data["rocket"]
+    F_NOM_ROCK = requests.get(ADRESS2).json()['name']
+
+    ADRESS3 = "https://api.spacexdata.com/v4/launchpads/" +\
+        data["launchpad"]
+    REQUTTE = requests.get(ADRESS3).json()
+    F_NOM_LUNCH = REQUTTE['name']
+    F_LUNCH_LOC = REQUTTE['locality']
+
+    print("{} ({}) {} - {} ({})".format(F_NOM, F_tmp_LOC,
+                                        F_NOM_ROCK, F_NOM_LUNCH,
+                                        F_LUNCH_LOC))
