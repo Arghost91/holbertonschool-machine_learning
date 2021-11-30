c41c84eebc1c8de85cb6926b2adf3ce4121bb5c4
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-11-30 01:39:07 +0100

    Create 1-sentience.py

---
+++ pipeline/0x01-apis/1-sentience.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Method that returns the list of names of the
+home planets of all sentient species.
+"""
+import requests
+
+
+def sentientPlanets():
+    """
+    * Prototype: def sentientPlanets():
+    * Don’t forget the pagination
+    """
+    url = "https://swapi-api.hbtn.io/api/species/"
+    r = requests.get(url)
+    planets = []
+    while(r.status_code == 200):
+        json_result = r.json()["results"]
+        for specie in json_result:
+            if specie["designation"] == "sentient" or
+            specie["classification"]:
+                if specie["homeworld"] is not None:
+                    url_plan = specie["homeworld"]
+                    planets.append(request.get(url_plan).json()
+                                   ["name"])
+        next = r.json()["next"]
+        if(next is None):
+            break
+        r = requests.get(next)
+    return planets
