1ce91976a723fdb8e11cf220edca0382921ef8ea
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-11-30 04:54:02 +0100

    Update 3-upcoming.py

--- pipeline/0x01-apis/3-upcoming.py
+++ pipeline/0x01-apis/3-upcoming.py
@@ -1,31 +1,34 @@
 #!/usr/bin/env python3
 """
-Script that displays the upcoming launch with these information
+Displays the upcoming launch information
 """
 import requests
 
-
 if __name__ == '__main__':
-    r = requests.get("https://api.spacexdata.com/v4/launches/upcoming")
-    r_get = r.json()
-    dates = [i['date_unix'] for i in r_get]
+    url = "https://api.spacexdata.com/v4/launches/upcoming"
+    r = requests.get(url)
+    json = r.json()
+
+    dates = [x['date_unix'] for x in json]
     index = dates.index(min(dates))
-    next = r_get[index]
-    name = next["name"]
-    date_local = next["date_local"]
-    rocket = next["rocket"]
-    rocket_url = "https://api.spacexdata.com/v4/rockets" + rocket
-    r_rocket = requests.get(rocket_url)
-    r_rocket_get = r_rocket.json()
-    rocket_name = r_rocket_get["name"]
-    launchpad = next["launchpad"]
-    launchpad_url = "https://api.spacexdata.com/v4/launchpads" + launchpad
-    r_launchpad = requests.get(launchpad_url)
-    r_launchpad_get = r_launchpad.json()
-    launchpad_name = r_launchpad_get["name"]
-    launchpad_locality = r_launchpad_get["locality"]
-print("{} ({}) {} - {} ({})".format(name,
-                                   date_local,
-                                   rocket_name,
-                                   launchpad_name,
-                                   launchpad_locality))
+    next_launch = json[index]
+
+    name = next_launch['name']
+    date = next_launch['date_local']
+    rocket_id = next_launch['rocket']
+    launchpad_id = next_launch['launchpad']
+
+    url_r = "https://api.spacexdata.com/v4/rockets/" + rocket_id
+    req_r = requests.get(url_r)
+    json_r = req_r.json()
+    rocket_name = json_r['name']
+
+    url_l = "https://api.spacexdata.com/v4/launchpads/" + launchpad_id
+    req_l = requests.get(url_l)
+    json_l = req_l.json()
+    launchpad_name = json_l['name']
+    launchpad_loc = json_l['locality']
+
+    info = (name + ' (' + date + ') ' + rocket_name + ' - ' +
+            launchpad_name + ' (' + launchpad_loc + ')')
+    print(info)
