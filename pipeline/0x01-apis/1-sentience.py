0185a0992b3ded41a65b942dd0ee38b32efb22a6
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-11-30 01:51:19 +0100

    Update 1-sentience.py

--- pipeline/0x01-apis/1-sentience.py
+++ pipeline/0x01-apis/1-sentience.py
@@ -1,38 +1,21 @@
 #!/usr/bin/env python3
-"""script for getting info from web pages
-"""
+"""Return a list of ships"""
+
 
 import requests
 
 
 def sentientPlanets():
-    """[Returns the list of names of the home planets of all sentient species]
-    Args:
-        None
-    Returns
-        The list of planets
-    """
-
+    """Return a list of ships"""
     planets = []
-    url = 'https://swapi-api.hbtn.io/api/species/'
-    response = requests.get(url)  # No authentication is required
-    r_code = response.status_code
-    next_url = url
-    while(r_code == 200):
-
-        json_data = response.json()['results']
-
-        for specie in json_data:
-            if(specie['designation'] == "sentient" or
-               specie["classification"].lower() == "sentient"):
-                url_planet = specie['homeworld']
-                if (url_planet is not None):
-                    planets.append(requests.get(url_planet).json()['name'])
-
-        next_url = response.json()['next']
-        if(next_url is None):
-            break
-
-        response = requests.get(next_url)
-
-    return (planets)
+    url = 'https://swapi-api.hbtn.io/api/species'
+    while url is not None:
+        data = requests.get(url).json()
+        for species in data['results']:
+            if ((species['designation'] == 'sentient'
+                 or species['designation'] == 'reptilian')):
+                if species['homeworld'] is not None:
+                    hw = requests.get(species['homeworld']).json()
+                    planets.append(hw['name'])
+        url = data['next']
+    return planets
