01bf9a8c3c8b396a6a62f08c4035c65c3d534ab0
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-11-30 05:40:47 +0100

    Create 4-rocket_frequency.py

---
+++ pipeline/0x01-apis/4-rocket_frequency.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Script that displays the number of launches per rocket
+"""
+import requests
+
+
+if __name__ == '__main__':
+    r = requests.get("https://api.spacexdata.com/v4/launches/")
+    rockets = {}
+    r_get = r.json()
+    for launch in launches:
+        rocket = launch["rocket"]
+        rocket_url = "https://api.spacexdata.com/v4/rockets/" + rocket
+        r_rocket = requests.get(rocket_url)
+        r_rocket_get = r_rocket.json()
+        rocket_name = r_rocket_get["name"]
+        if rocket in rockets.keys():
+            rockets[rocket] = rockets[rocket] + 1
+        else:
+            rockets[rocket] = 1
+    sorting = sorted(rockets.item(), key=lambda x: x[0])
+    sorting = sorted(sorting, key=lambda x: x[1], reverse=True)
+    for i in sorting:
+        print("{}: {}".format(i[0], i[1]))
