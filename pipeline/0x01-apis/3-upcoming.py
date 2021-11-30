b711d00f4a64fdb17d24eb27607573611a0f893d
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2021-11-30 05:18:15 +0100

    Update 3-upcoming.py

--- pipeline/0x01-apis/3-upcoming.py
+++ pipeline/0x01-apis/3-upcoming.py
@@ -1,53 +1,29 @@
 #!/usr/bin/env python3
-"""
-By using the (unofficial) SpaceX API write a script that displays the upcoming
-launch with these information:
-Name of the launch
-The date (in local time)
-The rocket name
-The name (with the locality) of the launchpad
-Format:
-<launch name> (<date>) <rocket name> - <launchpad name> (<launchpad locality>)
-The “upcoming launch” is the one which is the soonest from now, in UTC (we
-encourage you to use the date_unix for sorting it) - and if 2 launches have
-the same date, use the first one in the API result.
-"""
+"""Display soonest upcoming launch"""
+
 
 import requests
 
 
 if __name__ == '__main__':
-
-    base_url = "https://api.spacexdata.com/v4"
-    upcoming_launch_url = base_url + "/launches/upcoming"
-    response = requests.get(upcoming_launch_url)
-
-    r_code = response.status_code
-
-    if(r_code == 200):
-        next_date = response.json()[0]['date_unix']
-        launches_dict = response.json()
-
-        # find the date, name, local date and rocket of the next launch date
-        launch_date = [launch["date_unix"] for launch in launches_dict]
-        min_date = min(launch_date)
-        i = launch_date.index(min_date)
-
-        launch_name = launches_dict[i]["name"]  # 1
-        launch_date_local = launches_dict[i]["date_local"]  # 2
-        launch_rocket_number = str(launches_dict[i]["rocket"])
-        launchpad_number = str(launches_dict[i]["launchpad"])
-
-        rockets_url = base_url + "/rockets/" + launch_rocket_number
-        rocket_name = requests.get(rockets_url).json()["name"]  # 3
-
-        launchpad_url = base_url + "/launchpads/" + launchpad_number
-        launchpad_dict = requests.get(launchpad_url).json()
-        launchpad_name = launchpad_dict["name"]  # 4
-        launchpad_locality = launchpad_dict["locality"]  # 5
-
-    print("{} ({}) {} - {} ({})".format(launch_name,
-                                        launch_date_local,
-                                        rocket_name,
-                                        launchpad_name,
-                                        launchpad_locality))
+    data = requests.get('https://api.spacexdata.com/v4/launches/upcoming',
+                        headers={'pagination': 'false'})
+    data = data.json()
+    time = 99999999999
+    next = None
+    for launch in data:
+        thistime = int(launch['date_unix'])
+        if thistime < time:
+            time = thistime
+            next = launch
+    if next is not None:
+        rocket = requests.get('https://api.spacexdata.com/v4/rockets/'
+                              + next['rocket'])
+        rocket = rocket.json()['name']
+        lpad = requests.get('https://api.spacexdata.com/v4/launchpads/'
+                            + next['launchpad'])
+        lpad = lpad.json()
+        locale = lpad['locality']
+        lpad = lpad['name']
+        print('{} ({}) {} - {} ({})'.format(next['name'], next['date_local'],
+                                            rocket, lpad, locale))
