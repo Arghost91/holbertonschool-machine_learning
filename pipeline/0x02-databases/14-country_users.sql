0f83e3a2ba24ce406d6b320b2f96c16f34973fa8
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2022-01-30 22:27:07 +0100

    Update 14-country_users.sql

--- pipeline/0x02-databases/14-country_users.sql
+++ pipeline/0x02-databases/14-country_users.sql
@@ -10,6 +10,6 @@ CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
-    coutry ENUM('US', 'CO', 'TN') NOT NULL,
+    country ENUM('US', 'CO', 'TN') NOT NULL,
     PRIMARY KEY (id)
     );
