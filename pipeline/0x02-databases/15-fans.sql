92d06dc1dd6b013f16142ce03eb5bac00426fcb5
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2022-01-30 22:41:50 +0100

    Update 15-fans.sql

--- pipeline/0x02-databases/15-fans.sql
+++ pipeline/0x02-databases/15-fans.sql
@@ -2,7 +2,7 @@
 --    Import this table dump: metal_bands.sql.zip
 --    Column names must be: origin and nb_fans
 --    Your script can be executed on any database
-SELECT origin, nb_fans
+SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
