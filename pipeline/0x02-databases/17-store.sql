5216286bee3b586d86774ffc461543b94129ba30
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2022-01-30 23:20:06 +0100

    Create 17-store.sql

---
+++ pipeline/0x02-databases/17-store.sql
@@ -0,0 +1,7 @@
+--  Write a SQL script that creates a trigger that decreases the quantity of an item after adding a new order.
+--  Quantity in the table items can be negative.
+CREATE TRIGGER decreases AFTER INSERT ON orders
+FOR EACH ROW
+  UPDATE items
+  SET items.quantity = items.quantity - new_order
+  WHERE items.name = NEW.item_name;
