d83ee5d4295536ce95dddfb44bcfc42c0f4bd7c4
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2022-02-03 17:05:21 +0100

    Create 103-average_weighted_score.sql

---
+++ pipeline/0x02-databases/103-average_weighted_score.sql
@@ -0,0 +1,3 @@
+--  script that creates a stored procedure ComputeAverageWeightedScoreForUser that computes and store the average weighted score for a student.
+SELECT AVG(score) AS average
+FROM second_table;
