ae9c51b8b204057b94aa4e28dcbfb2d27956d8f4
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2022-02-03 17:12:32 +0100

    Update 103-average_weighted_score.sql

--- pipeline/0x02-databases/103-average_weighted_score.sql
+++ pipeline/0x02-databases/103-average_weighted_score.sql
@@ -1,3 +1,13 @@
 --  script that creates a stored procedure ComputeAverageWeightedScoreForUser that computes and store the average weighted score for a student.
-SELECT AVG(score) AS average
-FROM second_table;
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser (IN new_user_id INT)
+BEGIN
+  UPDATE user
+  SET average_score = (
+    SELECT SUM(weight * score) / SUM(weight)
+    FROM corrections, projects
+    WHERE corrections.user_id = user_id
+    AND corrections.project_id = projects_id)
+    WHERE id = user_id;
+END $$
+DELIMITER ;
