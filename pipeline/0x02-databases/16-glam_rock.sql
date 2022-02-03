3ef11c19a25e5cc4b2c6017c671e76566052dfb3
Arghost91 <81881440+Arghost91@users.noreply.github.com>
2022-02-03 16:00:31 +0100

    Update 16-glam_rock.sql

--- pipeline/0x02-databases/16-glam_rock.sql
+++ pipeline/0x02-databases/16-glam_rock.sql
@@ -3,7 +3,7 @@
 --    Column names must be: band_name and lifespan (in years)
 --    You should use attributes formed and split for computing the lifespan
 --    Your script can be executed on any database
-SELECT band_name, IF(split IS NULL, (2022 - formed), (split - formed)) AS lifespan
+SELECT band_name, IF(split IS NULL, (2021 - formed), (split - formed)) AS lifespan
 FROM metal_bands
 WHERE style REGEXP "Glam rock"
 ORDER BY lifespan DESC;
